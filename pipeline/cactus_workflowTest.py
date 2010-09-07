9aa23d323d3ddd85b50e30ba4572befe38692dcd
benedict <benedict@a8237c23-d22b-0410-ac1a-db40f18c2caa>
2010-09-07 20:19:03 +0000

    git-svn-id: svn+ssh://hgwdev.cse.ucsc.edu/projects/compbio/svnroot/hausslerlab/recon/branches/2009-09-16/src/cactus@6602 a8237c23-d22b-0410-ac1a-db40f18c2caa

--- pipeline/cactus_workflowTest.py
+++ pipeline/cactus_workflowTest.py
@@ -25,7 +25,7 @@ class TestCase(unittest.TestCase):
         
     def testCactus_Random(self):
         runWorkflow_multipleExamples(getCactusInputs_random, 
-                                     testNumber=TestStatus.getTestSetup(),
+                                     testNumber=0, #TestStatus.getTestSetup(),
                                      buildReference=False,
                                      batchSystem=self.batchSystem)
         
