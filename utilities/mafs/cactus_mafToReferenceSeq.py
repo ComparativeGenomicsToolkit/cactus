e755198eb85a10b225fd899233600c401a3f5e2b
nknguyen <nknguyen@a8237c23-d22b-0410-ac1a-db40f18c2caa>
2010-09-24 22:55:00 +0000

    sep 23 2010: nknguyen
    AM   mafs/cactus_mafToReferenceSeq.py (get the reference fasta sequence from maf file)
    M    mafs/Makefile 
    
    
    git-svn-id: svn+ssh://hgwdev.cse.ucsc.edu/projects/compbio/svnroot/hausslerlab/recon/branches/2009-09-16/src/cactus@6774 a8237c23-d22b-0410-ac1a-db40f18c2caa

---
+++ utilities/mafs/cactus_mafToReferenceSeq.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+#nknguyen@soe.ucsc.edu
+#09/24/2010
+#
+#Get the reference sequence from maf output of cactus_MAFGenerator (with 
+#options --orderByReference and --includeReferenceSequence specified)
+#Input: cactus.maf Output refseq.fa
+#Given that blocks are in order of the 'reference' in cactus.maf, and 'reference'
+#is always positive.
+
+import os
+import sys
+import re
+from optparse import OptionParser
+
+def getRefSeq():
+    seq = ''
+    #count = 0
+    #totalbases = 0
+    #go through input file (stdin) and get the sequences
+    for line in sys.stdin.readlines():
+        p = re.compile('^s\treference')
+        if p.match(line):
+            #0   1      2      3       4         5          6
+            #s, name, start, length, strand, totalLength, sequence
+            list = line.rstrip().split('\t')
+	    if len(list) != 7:
+	        sys.stderr.write("Line %s has wrong format\n" %line)
+	        continue
+            else:
+                seq += list[6]
+                #count += 1
+                #totalbases += int(list[3])
+    sys.stdout.write(">Reference\n")
+    #sys.stderr.write("Count: %d; totalBase: %d; Total bases in the ref: %d\n" %(count, totalbases, len(seq)))
+    for i in range(0, len(seq), 50):
+        sys.stdout.write(seq[i:i+50] + '\n')
+
+if __name__ == "__main__" :
+    usage = "usage: %prog [options] < input maf file > output fasta file"
+    parser = OptionParser(usage=usage)
+    (options, args) = parser.parse_args()
+    getRefSeq()
+
