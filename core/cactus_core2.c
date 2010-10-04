d2a98c2508d07b6e686e1a7463ff465233417288
benedict <benedict@a8237c23-d22b-0410-ac1a-db40f18c2caa>
2010-10-04 17:48:43 +0000

    Incomplete - task : speed everything up
    git-svn-id: svn+ssh://hgwdev.cse.ucsc.edu/projects/compbio/svnroot/hausslerlab/recon/branches/2009-09-16/src/cactus@6853 a8237c23-d22b-0410-ac1a-db40f18c2caa

--- core/cactus_core2.c
+++ core/cactus_core2.c
@@ -178,7 +178,7 @@ int main(int argc, char *argv[]) {
     // (0) Check the inputs.
     ///////////////////////////////////////////////////////////////////////////
 
-    //assert(logLevelString == NULL || strcmp(logLevelString, "CRITICAL") == 0 || strcmp(logLevelString, "INFO") == 0 || strcmp(logLevelString, "DEBUG") == 0);
+    assert(logLevelString == NULL || strcmp(logLevelString, "CRITICAL") == 0); // || strcmp(logLevelString, "INFO") == 0 || strcmp(logLevelString, "DEBUG") == 0);
     assert(alignmentsFile != NULL);
     assert(cactusDiskDatabaseString != NULL);
     assert(flowerName != NULL);
