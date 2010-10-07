6de21748bf4deb3a4a35835fb89b12751c903a50
benedict <benedict@a8237c23-d22b-0410-ac1a-db40f18c2caa>
2010-10-07 05:42:45 +0000

    Incomplete - task : push dent's smaller simulation through pipeline
    git-svn-id: svn+ssh://hgwdev.cse.ucsc.edu/projects/compbio/svnroot/hausslerlab/recon/branches/2009-09-16/src/cactus@6887 a8237c23-d22b-0410-ac1a-db40f18c2caa

--- pipeline/cactus_workflow.py
+++ pipeline/cactus_workflow.py
@@ -353,7 +353,7 @@ class CactusReferenceDown(Target):
     """This target does the down pass for the reference phase.
     """
     def __init__(self, options, flowerNames):
-        Target.__init__(self, time=3.0)
+        Target.__init__(self, time=1.0)
         self.options = options
         self.flowerNames = flowerNames
     
