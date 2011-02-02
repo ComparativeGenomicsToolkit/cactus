592029cf173ff517833b8b4a37657e6a5407f858
Benedict <benedictpaten@gmail.com>
2011-02-01 17:39:43 -0800

    Fixed build for module

--- Makefile
+++ Makefile
@@ -1,15 +1,7 @@
-# order is important, libraries first
-modules = api threeEdgeConnected pinchGraphs core setup blastAlignment baseAlignment normalisation reference phylogeny faces check pipeline utilities pulldown
-.PHONY: all %.all clean %.clean
+all :
+	cd src && make all
 
-all : ${modules:%=all.%}
-
-all.%:
-	cd $* && make all
-
-clean:  ${modules:%=clean.%}
-
-clean.%:
-	cd $* && make clean
+clean :
+	cd src && make clean
 
 
---
+++ include.mk
@@ -0,0 +1,22 @@
+#Location of sonLib
+binPath=${rootPath}bin
+libPath=${rootPath}lib
+sonLibRootPath=${rootPath}../sonLib
+sonLibPath=${sonLibRootPath}/lib
+
+include  ${sonLibRootPath}/include.mk
+
+dataSetsPath=/Users/benedictpaten/Dropbox/Documents/work/myPapers/genomeCactusPaper/dataSets
+
+cflags += -I ${sonLibPath}
+dblibs = ${tokyoCabinetLib} ${mysqlLibs} ${pgsqlLibs}
+basicLibs = ${sonLibPath}/sonLib.a ${sonLibPath}/cutest.a ${dblibs}
+basicLibsDependencies = ${sonLibPath}/sonLib.a ${sonLibPath}/cutest.a 
+
+# optional kent library stuff
+KENTDIR=/hive/groups/recon/local/kent/src
+ifneq ($(wildcard ${KENTDIR}),)
+    kentInc = ${KENTDIR}/inc
+    kentLib = ${KENTDIR}/lib
+    kentLibWeb = ${kentLib}/${MACH}/jkweb.a
+endif
