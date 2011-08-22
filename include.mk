bd108e29e6ad8f5c8931ee8ceb2de7c01f27dd20
Glenn Hickey <hickey@soe.ucsc.edu>
2011-08-22 14:00:08 -0700

    revert to default datasets path
    Change-Id: I7e8af7698e4ce92b7bf30b4ec52c25e36cceac5a

--- include.mk
+++ include.mk
@@ -8,7 +8,7 @@ sonLibPath=${sonLibRootPath}/lib
 
 include  ${sonLibRootPath}/include.mk
 
-dataSetsPath=/Users/hickey/Documents/Devel/genomes/dataSets
+dataSetsPath=/Users/benedictpaten/Dropbox/Documents/work/myPapers/genomeCactusPaper/dataSets
 
 cflags += -I ${sonLibPath} ${tokyoCabinetIncl} ${kyotoTycoonIncl} ${mysqlIncl} ${pgsqlIncl}
 basicLibs = ${sonLibPath}/sonLib.a ${sonLibPath}/cuTest.a ${dblibs}
