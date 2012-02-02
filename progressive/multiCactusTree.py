89e5f8fbff39f56fef3b5961d9ba8e39af911098
Glenn Hickey <hickey@soe.ucsc.edu>
2012-02-02 10:23:31 -0800

    fix getSubtreeRoot so it is consistent (returns node id all the time)

--- progressive/multiCactusTree.py
+++ progressive/multiCactusTree.py
@@ -95,12 +95,12 @@ class MultiCactusTree(NXTree):
         return mcCpy
         
     # find the root of the subtree containing the given node
-    # as leaf (slowly.. for nwo)
+    # as leaf (slowly.. for nwo) (returns event name, not node id)
     def getSubtreeRoot(self, name):
         assert len(self.nameToId) > 0
         node = self.nameToId[name]
         if node == self.rootId:
-            return node
+            return name
         parent = self.getParent(node)
         while parent is not None:
             if parent in self.subtreeRoots:
