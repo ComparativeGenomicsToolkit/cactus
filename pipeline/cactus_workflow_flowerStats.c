6db02086e025958c0dd022cc9cc615dd1d6d25a2
Benedict <benedictpaten@gmail.com>
2012-02-21 10:15:50 -0800

    Adding more info to flower stats

--- pipeline/cactus_workflow_flowerStats.c
+++ pipeline/cactus_workflow_flowerStats.c
@@ -30,19 +30,24 @@ int main(int argc, char *argv[]) {
     int32_t totalGroups = flower_getGroupNumber(flower);
     int32_t maxEndDegree = 0;
     int64_t maxAdjacencyLength = 0;
+    int32_t totalEdges = 0;
 
     Flower_EndIterator *endIt = flower_getEndIterator(flower);
     End *end;
     while((end = flower_getNextEnd(endIt)) != NULL) {
+        assert(end_getOrientation(end));
         if(end_getInstanceNumber(end) > maxEndDegree) {
             maxEndDegree = end_getInstanceNumber(end);
         }
+        stSortedSet *ends = stSortedSet_construct();
         End_InstanceIterator *capIt = end_getInstanceIterator(end);
         Cap *cap;
         while((cap = end_getNext(capIt)) != NULL) {
             if(cap_getSequence(cap) != NULL) {
                 Cap *adjacentCap = cap_getAdjacency(cap);
                 assert(adjacentCap != NULL);
+                End *adjacentEnd = end_getPositiveOrientation(cap_getEnd(adjacentCap));
+                stSortedSet_insert(ends, adjacentEnd);
                 int64_t adjacencyLength = cap_getCoordinate(cap) - cap_getCoordinate(adjacentCap);
                 if(adjacencyLength < 0) {
                     adjacencyLength *= -1;
@@ -53,10 +58,17 @@ int main(int argc, char *argv[]) {
                 }
             }
         }
+        end_destructInstanceIterator(capIt);
+        totalEdges += stSortedSet_size(ends);
+        if(stSortedSet_search(ends, end) != NULL) { //This ensures we count self edges twice, so that the division works.
+            totalEdges += 1;
+        }
+        stSortedSet_destruct(ends);
     }
+    assert(totalEdges % 2 == 0);
 
-    printf("total bases: %" PRIi64 " total-ends: %i total-caps: %i max-end-degree: %i max-adjacency-length: %" PRIi64 " total-blocks: %i total-groups: %i\n",
-            totalBases, totalEnds, totalCaps, maxEndDegree, maxAdjacencyLength, totalBlocks, totalGroups);
+    printf("total bases: %" PRIi64 " total-ends: %i total-caps: %i max-end-degree: %i max-adjacency-length: %" PRIi64 " total-blocks: %i total-groups: %i total-edges: %i\n",
+            totalBases, totalEnds, totalCaps, maxEndDegree, maxAdjacencyLength, totalBlocks, totalGroups, totalEdges);
 
     return 0;
 }
