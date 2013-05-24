99a0d3ef4aba44a94b62357dfdf0361b09f4e33a
Glenn Hickey <hickey@soe.ucsc.edu>
2013-05-24 14:45:06 -0700

    change progressive scheduler to use jobtrees follow-on option -- ie probably break everything.  will need to go back and speed up trans reduction for larger  trees

--- progressive/schedule.py
+++ progressive/schedule.py
@@ -67,42 +67,147 @@ class Schedule:
     
     # break all the cycles in reverse topological order
     def compute(self):
+        def getName(pref, id):
+            while '%s%d' % (pref, id) in self.depTree:
+                id += 1
+            return '%s%d' % (pref, id)        
         self.depTree = self.inGraph.copy()
-        self.enforceMaxParallel()
+        self.transitveReduction(self.depTree)
         tsort = NX.topological_sort(self.depTree)
         tsort.reverse()
+        nextId = 0
         for node in tsort:
+            #strip out cycles within parents
+            parents = set([i[0] for i in self.depTree.in_edges(node)])
+            if len(parents) > 1:
+                for parent in parents:
+                    for pedge in self.depTree.out_edges(parent):
+                        if pedge[1] in parents:
+                            self.depTree.remove_edge(parent, node)
+                            break
+                        
             parents = [i[0] for i in self.depTree.in_edges(node)]
             if len(parents) > 1:
-                chosenParentIndex = min([tsort.index(i) for i in parents])
-                chosenParent = tsort[chosenParentIndex]
+                # insert new "virtual" node as parent to all parents
+                vpNode = getName("Virtual", nextId)
+                self.depTree.add_node(vpNode, virtual='1')
+                # insert new "virtual follow-on" node as follow up to above node
+                vfoNode = getName("VirtualF", nextId)
+                self.depTree.add_node(vfoNode, virtual='1')
+                self.depTree.add_edge(vpNode, vfoNode, followOn='1')
+
+                nextId += 1
                 for parent in parents:
-                    if parent != chosenParent:
-                        self.depTree.remove_edge(parent, node)
-                        if chosenParent not in self.deps(parent):
-                            self.depTree.add_edge(parent, chosenParent)
+                    # insert vpNode above all the parents
+                    for inEdge in self.depTree.in_edges(parent):
+                        if not self.isFollowOn(inEdge[0], inEdge[1]):
+                            self.depTree.add_edge(inEdge[0], vpNode)
+                            self.depTree.remove_edge(inEdge[0], inEdge[1])
+                    # add parent as child of the followOn
+                    self.depTree.add_edge(vfoNode, parent)
+                    # remove node from parent
+                    self.depTree.remove_edge(parent, node)
+                    # add parent's childrent to vpNode
+                    for child in self.depTree.successors(parent):
+                        if not self.isFollowOn(parent, child):
+                            self.depTree.remove_edge(parent, child)
+                            self.depTree.add_edge(vpNode, child)
+                            if self.depTree.has_edge(child, vpNode):
+                                self.depTree.remove_edge(child, vpNode)
+                #add node to vpNode
+                self.depTree.add_edge(vpNode, node)
+                #lazy clean-up of inserted transitive edges
+                #will need to not do this brute-force for bigger trees
+                self.transitveReduction(self.depTree)
+                assert(len(self.depTree.in_edges(node)) == 1)
+                # process virtual node next
+                tsort.insert(tsort.index(node) + 1, vpNode)
+                
             assert len(self.depTree.in_edges(node)) < 2
         
         for node in tsort:
             assert len(self.depTree.in_edges(node)) < 2
         assert NX.is_directed_acyclic_graph(self.depTree)
-        
+        self.enforceMaxParallel()
+
+    # friday afternoon!
+    def transitveReduction(self, digraph):
+        paths = NX.all_pairs_shortest_path(digraph)
+        def hasPath(node1, node2):
+            if node1 == node2:
+                return False
+            return (node1 in paths and node2 in paths[node1] and
+                    len(paths[node1][node2]) > 0)
+        for x in digraph.nodes():
+            for y in digraph.nodes():
+                for z in digraph.nodes():
+                    if (x != z and digraph.has_edge(x, z) and hasPath(x, y) and
+                        hasPath(y, z)):
+                        digraph.remove_edge(x, z)
+                        
+                        
     # add dependencies to ensure that more than self.maxParallelSubtrees
     # different jobs can never be scheduled at the same time
     def enforceMaxParallel(self):
-        if self.maxParallelSubtrees is None:
+        if self.maxParallelSubtrees is None or len(self.depTree.nodes()) <= 2:
             return
-        leaves = []
-        for node in self.depTree.nodes():
-            if len(self.depTree.out_edges(node)) == 0:
-                leaves.append(node)
-        while len(leaves) > self.maxParallelSubtrees:
-            assert len(leaves) > 1
-            newTarget = leaves[0]
-            newSource = leaves[-1]
-            del leaves[-1]
-            self.depTree.add_edge(newSource, newTarget)
-                        
+        assert self.maxParallelSubtrees > 0
+        tree = self.depTree.copy()
+        # remove followOn edges
+        for edge in tree.edges():
+            if self.isFollowOn(edge[0], edge[1]):
+                tree.remove_edge(edge[0], edge[1])
+        # get roots
+        roots = []
+        for node in tree.nodes():
+            if len(tree.in_edges(node)) == 0:
+                roots.append(node)
+        # for each root, independently reduce the number of leaves
+        for root in roots:
+            leaves = []
+            for node in NX.bfs_tree(tree, root):
+                if (len(tree.out_edges(node)) == 0 and
+                    not self.isVirtual(node)):
+                    leaves.append(node)
+            numLeaves = len(leaves)
+            # sort leaves by chain length (lowest at end)
+            leafChains = [self.getChainParent(tree, x) for x in leaves]
+            leafChains.sort()
+            leaves = [x[1] for x in leafChains]
+            leaves.reverse()
+            while numLeaves > self.maxParallelSubtrees:
+                # try to insert the last leaf of the list above
+                # its sibling
+                leaf = leaves[-1]
+                chainParent = self.getChainParent(tree, leaf)[2]
+                parents = [x for x in tree.predecessors(chainParent)]
+                assert len(parents) == 1
+                parent = parents[0]
+                children = [x for x in tree.successors(parent)]
+                for child in children:
+                    if child != chainParent and chainParent != root:
+                        tree.add_edge(leaf, child)
+                        self.depTree.add_edge(leaf, child)
+                        tree.remove_edge(parent, child)
+                        self.depTree.remove_edge(parent, child)
+                        numLeaves -= 1
+                        break
+                del leaves[-1]
+            assert numLeaves <= self.maxParallelSubtrees
+
+    # walk up a path (not bifurcations) from a leaf as far as we can
+    def getChainParent(self, tree, node):
+        chainLen = 0
+        chainParent = node
+        while True:
+            parents = tree.predecessors(chainParent)
+            if len(parents) == 1 and len(tree.successors(parents[0])) == 1:
+                chainParent = parents[0]
+                chainLen += 1
+            else:
+                break
+        return (chainLen, node, chainParent)
+        
     # for a given event name, get the names of all the 
     # events that are directly dependent on it in the 
     # schedule
@@ -110,9 +215,33 @@ class Schedule:
         assert name in self.depTree        
         edges = self.depTree.out_edges(name)
         depList = []
-        for edge in edges: 
-            depList.append(edge[1])
+        for edge in edges:
+            if not self.isFollowOn(edge[0], edge[1]):
+                depList.append(edge[1])
         return depList
+
+    # get the follow on node if it exists
+    def followOn(self, name):
+        assert name in self.depTree
+        edges = self.depTree.out_edges(name)
+        depList = []
+        for edge in edges:
+            if self.isFollowOn(edge[0], edge[1]):
+                depList.append(edge[1])
+        assert len(depList) < 2
+        if len(depList) == 1:
+            return depList[0]
+        return None
+
+    # test if an edge is a follow on 
+    def isFollowOn(self, parent, child):
+        edge = self.depTree[parent][child]
+        return 'followOn' in edge and str(edge['followOn']) == '1'
+    
+    # test if node corresponds to genome event or was added to break a virtual
+    def isVirtual(self, name):
+        node = self.depTree.node[name]
+        return 'virtual' in node and str(node['virtual']) == '1'
     
     # write to graphviz dot file
     def writeToFile(self, path):
