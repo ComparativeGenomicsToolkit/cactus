f36b61d7bc525750a57dea99c82385ff05a62fbf
Benedict <benedictpaten@gmail.com>
2013-06-13 10:46:18 -0700

    -

--- reference/impl/buildReference.c
+++ reference/impl/buildReference.c
@@ -153,12 +153,15 @@ static int64_t calculateZP2(Cap *cap, stHash *endsToNodes) {
     Cap *otherCap = calculateZP4(cap, endsToNodes);
     int64_t capLength;
     if (otherCap == NULL) {
-        capLength = 1000000000; //make the length really long if attached, so that we don't bias toward one or the other end.
-        //capLength = cap_getSide(cap) ? sequence_getLength(sequence) + sequence_getStart(sequence) - cap_getCoordinate(cap)
-        //        : cap_getCoordinate(cap) - sequence_getStart(sequence) + 1;
+        //capLength = 1000000000; //make the length really long if attached, so that we don't bias toward one or the other end.
+        capLength = cap_getSide(cap) ? sequence_getLength(sequence) + sequence_getStart(sequence) - cap_getCoordinate(cap)
+                : cap_getCoordinate(cap) - sequence_getStart(sequence) + 1;
     } else {
     	capLength = cap_getSide(cap) ? cap_getCoordinate(otherCap) - cap_getCoordinate(cap) + 1 : cap_getCoordinate(cap)
                 - cap_getCoordinate(otherCap) + 1;
+    }
+    if(capLength == 0) {
+        capLength = 1;
     }
     assert(capLength > 0);
     return capLength;
