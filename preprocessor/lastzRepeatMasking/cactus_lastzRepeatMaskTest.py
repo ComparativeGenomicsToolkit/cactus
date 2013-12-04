8c2d56c8566144907671d65c9c74e8e18af5f0eb
Benedict <benedictpaten@gmail.com>
2013-12-04 10:57:07 -0800

    Making changes to default lastz repeat masking parameters.

--- preprocessor/lastzRepeatMasking/cactus_lastzRepeatMaskTest.py
+++ preprocessor/lastzRepeatMasking/cactus_lastzRepeatMaskTest.py
@@ -51,7 +51,7 @@ class TestCase(PreprocessorTestCase):
                  " lastz was filter for max-occurrences of more than : ", maxOccurrence
                  
                  #Run lastz repeat masker using heuristic settings for comparison with the slower settings
-                command = "cactus_lastzRepeatMask.py --proportionSampled=1.0 --minPeriod=%s --lastzOpts='--step=1 --ambiguous=iupac,100 --ungapped' --fragment=%s %s %s" % \
+                command = "cactus_lastzRepeatMask.py --proportionSampled=1.0 --minPeriod=%s --lastzOpts='--step=3 --ambiguous=iupac,100 --ungapped' --fragment=%s %s %s" % \
                        (maxOccurrence, 200, sequenceFile, self.tempOutputFile)
                 print "command to run (fast)", command
                 startTime = time.time()
