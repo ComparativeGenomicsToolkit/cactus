3f06a8ba4a54322f59241d63da692129fb3caf41
Joel Armstrong <jcarmstr@ucsc.edu>
2014-03-27 15:41:33 -0700

    Allow multiple outgroups
    (no xml option yet)

--- progressive/cactus_createMultiCactusProject.py
+++ progressive/cactus_createMultiCactusProject.py
@@ -129,13 +129,13 @@ def createFileStructure(mcProj, expTemplate, configTemplate, options):
         if configTemplate.getOutgroupStrategy() != 'none' \
         and name in mcProj.outgroup.ogMap \
         and name != mcProj.mcTree.getRootName():
-            og, ogDist = mcProj.outgroup.ogMap[name]
-            if og in expTemplate.seqMap:
-                ogPath = expTemplate.seqMap[og]
-            else:
-                ogPath = os.path.join(options.path, og)
-                ogPath = os.path.join(ogPath, refFileName(og))
-            exp.addTheOutgroupSequence(og, ogDist, ogPath)
+            for og, ogDist in mcProj.outgroup.ogMap[name]:
+                if og in expTemplate.seqMap:
+                    ogPath = expTemplate.seqMap[og]
+                else:
+                    ogPath = os.path.join(options.path, og)
+                    ogPath = os.path.join(ogPath, refFileName(og))
+                exp.addOutgroupSequence(og, ogDist, ogPath)
         elif name == mcProj.mcTree.getRootName() \
         and options.rootOutgroupPath is not None:
             exp.xmlRoot.attrib["outgroup_events"] = "rootOutgroup"
@@ -223,7 +223,7 @@ def main():
         # Add to tree
         mcProj.mcTree.addOutgroup("rootOutgroup", options.rootOutgroupDist)
         mcProj.mcTree.computeSubtreeRoots()
-        mcProj.outgroup.ogMap[mcProj.mcTree.getRootName()] = ("rootOutgroup", options.rootOutgroupDist)
+        mcProj.outgroup.ogMap[mcProj.mcTree.getRootName()] = [("rootOutgroup", options.rootOutgroupDist)]
     #Now do the file tree creation
     createFileStructure(mcProj, expTemplate, confTemplate, options)
    # mcProj.check()
