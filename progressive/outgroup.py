605c2b39823c972f04e938a0d5f6058c7d0119f5
Joel Armstrong <jcarmstr@ucsc.edu>
2014-11-29 12:22:37 +0000

    stop outgroups from being descendants of any other outgroup

--- progressive/outgroup.py
+++ progressive/outgroup.py
@@ -172,6 +172,11 @@ class GreedyOutgroup:
             htable[sink] - htable[source] + 1 > threshold:
                 continue
 
+            # Don't use any outgroups that are a child of another node
+            # already in the outgroup set
+            if any([self.onSamePath(x, sink) for x in self.dag.successors(source)]):
+                continue
+
             if source not in finished and \
             not self.onSamePath(source, sink):
                 self.dag.add_edge(source, sink, weight=dist, info='outgroup')
