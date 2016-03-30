b27b19b61bb7c55706b574fc91be54c2a7512f46
Joel Armstrong <jcarmstr@ucsc.edu>
2016-03-30 12:51:25 -0700

    final simple test to prove the telomere-adjacent recoverability works

--- caf/tests/recoverableChainsTest.c
+++ caf/tests/recoverableChainsTest.c
@@ -83,9 +83,58 @@ static void testRemovesIndel(CuTest *testCase) {
     testCommon_deleteTemporaryCactusDisk(cactusDisk);
 }
 
+// If the alignment looks like this, with = representing aligned columns:
+//
+// thread 4     =
+// thread 3   =-=-=
+// thread 2 =-=-=-=-=
+// thread 1 =-=-=-=-=
+//
+// then only the middle block should be kept. The older method would
+// keep all blocks as all are telomere-adjacent.
+static void testRecoverableTelomereAdjacentChainsNotKept(CuTest *testCase) {
+    CactusDisk *cactusDisk = testCommon_getTemporaryCactusDisk();
+    Flower *flower = flower_construct2(0, cactusDisk);
+    group_construct2(flower);
+    flower_check(flower);
+
+    Name thread1Name = addThreadToFlower(flower, 100);
+    Name thread2Name = addThreadToFlower(flower, 100);
+    Name thread3Name = addThreadToFlower(flower, 100);
+    Name thread4Name = addThreadToFlower(flower, 100);
+    stPinchThreadSet *threadSet = stCaf_setup(flower);
+    stPinchThread *thread1 = stPinchThreadSet_getThread(threadSet, thread1Name);
+    stPinchThread *thread2 = stPinchThreadSet_getThread(threadSet, thread2Name);
+    stPinchThread *thread3 = stPinchThreadSet_getThread(threadSet, thread3Name);
+    stPinchThread *thread4 = stPinchThreadSet_getThread(threadSet, thread4Name);
+
+    stPinchThread_pinch(thread1, thread2, 10, 10, 10, true);
+    stPinchThread_pinch(thread1, thread2, 20, 20, 10, true);
+    stPinchThread_pinch(thread1, thread3, 20, 20, 10, true);
+    stPinchThread_pinch(thread1, thread2, 30, 30, 10, true);
+    stPinchThread_pinch(thread1, thread3, 30, 30, 10, true);
+    stPinchThread_pinch(thread1, thread4, 30, 30, 10, true);
+    stPinchThread_pinch(thread1, thread2, 40, 40, 10, true);
+    stPinchThread_pinch(thread1, thread3, 40, 40, 10, true);
+    stPinchThread_pinch(thread1, thread2, 50, 50, 10, true);
+
+    // There should now be 13 blocks -- one for each cap, and the blocks we just added
+    CuAssertIntEquals(testCase, 13, stPinchThreadSet_getTotalBlockNumber(threadSet));
+    // Run the remove-recoverable-chains code
+    stCaf_meltRecoverableChains(flower, threadSet, true, 1000, NULL);
+    // Four blocks should have been removed
+    CuAssertIntEquals(testCase, 9, stPinchThreadSet_getTotalBlockNumber(threadSet));
+    // The middle block (30-40) should be the one that still exists
+    CuAssertTrue(testCase, stPinchSegment_getBlock(stPinchThread_getSegment(thread1, 35)) != NULL);
+
+    stPinchThreadSet_destruct(threadSet);
+    testCommon_deleteTemporaryCactusDisk(cactusDisk);
+}
+
 CuSuite *recoverableChainsTestSuite(void) {
     CuSuite *suite = CuSuiteNew();
     SUITE_ADD_TEST(suite, testDoesNotRemoveIsolatedChain);
     SUITE_ADD_TEST(suite, testRemovesIndel);
+    SUITE_ADD_TEST(suite, testRecoverableTelomereAdjacentChainsNotKept);
     return suite;
 }
