df0c24fbc84ee2de2a8955af8642fa932681c6a2
Joel Armstrong <jcarmstr@ucsc.edu>
2016-03-29 17:36:44 -0700

    simple indel unit test

--- caf/tests/recoverableChainsTest.c
+++ caf/tests/recoverableChainsTest.c
@@ -47,8 +47,45 @@ static void testDoesNotRemoveIsolatedChain(CuTest *testCase) {
     testCommon_deleteTemporaryCactusDisk(cactusDisk);
 }
 
+// If there are three threads, two of which share an indel, that indel
+// should be considered recoverable.
+static void testRemovesIndel(CuTest *testCase) {
+    CactusDisk *cactusDisk = testCommon_getTemporaryCactusDisk();
+    Flower *flower = flower_construct2(0, cactusDisk);
+    group_construct2(flower);
+    flower_check(flower);
+
+    Name thread1Name = addThreadToFlower(flower, 100);
+    Name thread2Name = addThreadToFlower(flower, 100);
+    Name thread3Name = addThreadToFlower(flower, 100);
+    stPinchThreadSet *threadSet = stCaf_setup(flower);
+    stPinchThread *thread1 = stPinchThreadSet_getThread(threadSet, thread1Name);
+    stPinchThread *thread2 = stPinchThreadSet_getThread(threadSet, thread2Name);
+    stPinchThread *thread3 = stPinchThreadSet_getThread(threadSet, thread3Name);
+    // Three 10-bp blocks separated by 20 bp. The two outer blocks
+    // involve all three threads, but the middle one only involves
+    // threads 1 and 2.
+    stPinchThread_pinch(thread1, thread2, 10, 10, 10, true);
+    stPinchThread_pinch(thread1, thread3, 10, 10, 10, true);
+    stPinchThread_pinch(thread1, thread2, 40, 40, 10, true);
+    stPinchThread_pinch(thread1, thread2, 70, 70, 10, true);
+    stPinchThread_pinch(thread1, thread3, 70, 70, 10, true);
+    // There should now be 9 blocks -- one for each cap, and the blocks we just added
+    CuAssertIntEquals(testCase, 9, stPinchThreadSet_getTotalBlockNumber(threadSet));
+    // Run the remove-recoverable-chains code
+    stCaf_meltRecoverableChains(flower, threadSet, true, 1000, NULL);
+    // One block should be missing
+    CuAssertIntEquals(testCase, 8, stPinchThreadSet_getTotalBlockNumber(threadSet));
+    // It should be the middle one (40-50)
+    CuAssertTrue(testCase, stPinchSegment_getBlock(stPinchThread_getSegment(thread1, 45)) == NULL);
+
+    stPinchThreadSet_destruct(threadSet);
+    testCommon_deleteTemporaryCactusDisk(cactusDisk);
+}
+
 CuSuite *recoverableChainsTestSuite(void) {
     CuSuite *suite = CuSuiteNew();
     SUITE_ADD_TEST(suite, testDoesNotRemoveIsolatedChain);
+    SUITE_ADD_TEST(suite, testRemovesIndel);
     return suite;
 }
