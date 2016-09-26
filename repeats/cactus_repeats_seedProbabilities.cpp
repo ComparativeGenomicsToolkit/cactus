1319b5eb38649c9d36aa46d958b19b276e3a3e9e
Alden <adderan@ucsc.edu>
2016-09-25 17:32:12 -0700

    Fix graph traversal

--- repeats/cactus_repeats_seedProbabilities.cpp
+++ repeats/cactus_repeats_seedProbabilities.cpp
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <vector>
 #include <map>
+#include <set>
 #include <iostream>
 #include <regex>
 #include <iomanip>
@@ -9,13 +10,9 @@
 
 #include "junctionapi/junctionapi.h"
 #include "streamfastaparser.h"
+#include "common.h"
 
-
-typedef std::map<int, int> JunctionMultiplicity;
-typedef std::map<int, int> PositionToJunction;
-typedef std::map<int, std::vector<int>> JunctionPositions;
-typedef std::map<std::string, double> SeedProbabilities;
-
+#define CONFIDENCE 0.9
 
 /*
 n: number of nodes
@@ -25,136 +22,12 @@ double edgeProbabilityToCollapseGraph(int n, double threshold) {
     return 1.0 - pow((1.0 - threshold)/(double)n, 1.0/((double)(n - 1)));
 }
 
-typedef struct Params {
-    int k = 19; //kmer length
-    int maxPathLength = 20; //terminate path after this length, regardless of weight
-    double pathThreshold = 1.25; //threshold of pathWeight/pathLength at which to terminate path
-    int scoreThreshold = 5; //Score threshold to include kmer in output
-    std::string seedPattern = "1110100110010101111";
-} Params;
-
-
-class CompactedVertex {
-    private:
-        int position;
-        std::map<int, int> edges;
-        int multiplicity;
-    public:
-        void addEdge(int toId);
-        int stepForward();
-        int getPosition() {return position;}
-        int getMultiplicity() {return multiplicity;}
-        CompactedVertex(int _position): position(_position) {}
-};
-
-int CompactedVertex::stepForward() {
-    int highestMultiplicity = 0;
-    int bestEdge = 0;
-    for (auto &kv : edges) {
-        if (kv.second > highestMultiplicity) {
-            highestMultiplicity = kv.second;
-            bestEdge = kv.first;
-        }
-    }
-    return bestEdge;
-}
-void CompactedVertex::addEdge(int id) {
-    edges[id] += 1;
-    multiplicity += 1;
-}
-
-class CompactedDeBruijn {
-    private:
-        std::map<int, Vertex*> vertices;
-        std::string sequence;
-        const Params &params;
-
-    public:
-        CompactedDeBruijn(const std::string &sequenceFilename, 
-                const std::string &graphFilename,
-                const Params &_params);
-        std::map<std::string, int> findHighMultiplicityPaths();
-
-};
-
-CompactedDeBruijn::CompactedDeBruijn(const std::string &sequenceFilename,
-        const std::string &graphFilename): params(_params) {
-    params = _params;
-    TwoPaCo::JunctionPosition pos;
-    TwoPaCo::JunctionPositionReader reader(graphFilename.c_str());
-
-
-    reader.NextJunctionPosition(pos);
-    int prevId = pos.GetId();
-    vertices[prevId] = new Vertex(pos.GetPos());
-    //Index junction IDs by position and position by junction ID
-    while (reader.NextJunctionPosition(pos)) {
-        int id = pos.GetId();
-        if (!vertices.count(id)) {
-            vertices[id] = new Vertex(pos.GetPos());
-        }
-        vertices[prevId].addEdge(id);
-
-
-    }
-
-    //Read the genome
-    TwoPaCo::StreamFastaParser parser(sequenceFilename);
-    char ch;
-    if (parser.ReadRecord()) {
-        while (parser.GetChar(ch)) {
-            sequence.push_back(ch);
-        }
-    }
-}
-
-std::map<std::string, int> findHighMultiplicityPaths() {
-    std::set<int> seen;
-    std::vector<int> path;
-    std::map<std::string, int> kmerMultiplicity;
-
-    Vertex* v = NULL;
-    for (auto &kv : vertices) {
-        int id = kv.first;
-        v = kv.second;
-        if(seen.find(v) != v.back()) continue;
-
-        //Try to extend a high-multiplicity path starting at
-        //this vertex
-        int pathWeight = 0;
-        int multiplicity = v->getMultiplicity();
-        while (multiplicity > params.minPathMultiplicity) {
-
-            //Don't traverse cycles
-            if(std::find(path, id) != path.back()) break;
-
-            path.push_back(id);
-            seen.insert(id);
-            pathWeight += multiplicity;
-
-
-            id = v->stepForward();
-            v = vertices[id];
-            multiplicity = v->getMultiplicity();
-
-        }
-        if (path.size() == 0) continue;
-
-        int pathStart = vertices[path[0]]->getPosition();
-        int pathStop = vertices[path.back()]->getPosition();
-        for (int i = pathStart; i < pathStop + params.k; i++) {
-            kmerMultiplicity[sequence.substr(i, params.k)] = pathWeight;
-        }
-        path.clear();
-
-    }
-}
 
 std::string kmerToSeed(std::string kmer, std::string seedPattern) {
     std::string seed;
     assert (kmer.length() == seedPattern.length());
     for (int i = 0; i < kmer.length(); i++) {
-        if (params.seedPattern[i] == '0') {
+        if (seedPattern[i] == '0') {
             seed += 'x';
         }
         else {
@@ -165,39 +38,148 @@ std::string kmerToSeed(std::string kmer, std::string seedPattern) {
     return seed;
 }
 
-std::map<std::string, unsigned long> parseLastzTable(const std::string &lastzFilename) {
-    std::map<std::string, unsigned long> seedToPacked;
-    std::string seed;
-    unsigned long packed;
-    unsigned int count;
 
-    std::ifstream lastzFile(lastzFilename);
-    std::string line;
-    const std::regex pattern("([0-9A-F]+)\\/([ACTGx]+):\\s+([0-9]+)");
-    std::stringstream ss;
-    std::smatch match;
-    while (std::getline(lastzFile, line)) {
-        std::regex_match(line, match, pattern);
-        if (match.size() < 3) continue;
-        ss.clear();
-        ss << std::hex << match[1];
-        ss >> packed;
-        ss.clear();
-        ss << match[2];
-        ss >> seed;
-        ss.clear();
-        ss << match[3];
-        ss >> count;
-        ss.clear();
+typedef struct Params {
+    int k = 19; //kmer length
+    int maxPathLength = 20; //terminate path after this length, regardless of weight
+    double minPathMultiplicity = 5;
+    int scoreThreshold = 5; //Score threshold to include kmer in output
+    std::string seedPattern = "1110100110010101111";
+} Params;
 
-        //std::cerr << "Parsed seed " << seed << " with packed representation " 
-        //    << packed << std::endl;
-        seedToPacked[seed] = packed;
+struct Edge {
+    int id;
+    int start;
+    int end;
+    int multiplicity;
+    std::vector<int> incidentEdges;
+
+};
+
+
+class CompactedDeBruijn {
+    private:
+        std::map<int, Edge*> edges;
+        const Params &params;
+        std::string genome;
+        std::ofstream *statsFile;
+
+    public:
+        CompactedDeBruijn(const std::string &graphFilename,
+            const std::vector<std::string> &genomes, const Params &_params,
+            std::ofstream *_statsFile);
+        std::map<std::string, double> findHighMultiplicityPaths(const std::string &seedPattern, std::map<std::string, std::string> &seedToPacked);
+
+};
+
+CompactedDeBruijn::CompactedDeBruijn(const std::string &graphFilename, 
+        const std::vector<std::string> &genomes, const Params &_params, 
+        std::ofstream *_statsFile): params(_params), statsFile(_statsFile) {
+
+    TwoPaCo::JunctionPosition end;
+    TwoPaCo::JunctionPosition begin;
+    ChrReader chrReader(genomes);
+    TwoPaCo::JunctionPositionReader reader(graphFilename.c_str());
+
+
+    chrReader.NextChr(genome);
+    Edge *prevEdge = NULL;
+    std::cerr << "Genome length = " << genome.length() << std::endl;
+    if (reader.NextJunctionPosition(begin)) {
+        while (reader.NextJunctionPosition(end)) {
+            Segment *segment = new Segment(begin, end, genome[begin.GetPos() + params.k],
+                    TwoPaCo::DnaChar::ReverseChar(genome[end.GetPos() - 1]));
+
+            Edge *edge;
+            if (!edges.count(segment->GetSegmentId())) {
+                edge = new Edge();
+                edges[segment->GetSegmentId()] = edge;
+                edge->id = segment->GetSegmentId();
+                edge->start = begin.GetPos();
+                edge->end = end.GetPos();
+            }
+            else {
+                edge = edges[segment->GetSegmentId()];
+            }
+            edge->multiplicity++;
+            if (prevEdge) {
+                prevEdge->incidentEdges.push_back(segment->GetSegmentId());
+            }
+            prevEdge = edge;
+            delete segment;
+
+            begin = end;
+        }
     }
-    return seedToPacked;
-
 }
 
+
+std::map<std::string, double> CompactedDeBruijn::findHighMultiplicityPaths(const std::string &seedPattern, std::map<std::string, std::string> &seedToPacked) {
+    std::map<std::string, double> seedProbabilities;
+
+    std::set<int> seen;
+
+
+    Edge *edge;
+    for (auto &kv : edges) {
+        int edgeId = kv.first;
+        edge = kv.second;
+
+        if(seen.count(edge->id)) continue;
+
+        //seen[edgeId] = true;
+
+        std::string segment;
+
+        //edges already seen on the path, to prevent cycles
+        std::string path;
+        std::set<int> pathEdges;
+        int pathWeight = 0;
+        while(edge->multiplicity > params.minPathMultiplicity) {
+            if (pathEdges.count(edge->id)) break;
+
+            std::string segmentString;
+            if (edge->id > 0) {
+                segment.assign(genome.substr(edge->start,
+                        (edge->end - edge->start) + params.k));
+            }
+            else {
+                segment.assign(TwoPaCo::DnaChar::ReverseCompliment(genome.substr(edge->start, (edge->end - edge->end) + params.k)));
+            }
+            std::cerr << "Appending " << segment << " to path" << std::endl;
+
+            path.append(segment);
+            pathEdges.insert(edge->id);
+            pathWeight += edge->multiplicity;
+
+            //Choose next segment to traverse
+            int maxMultiplicity = 0;
+            Edge *bestEdge = NULL;
+            for (int edgeID : edge->incidentEdges) {
+                Edge *candidateEdge = edges[edgeID];
+                if (candidateEdge->multiplicity > maxMultiplicity) {
+                    maxMultiplicity = candidateEdge->multiplicity;
+                    bestEdge = candidateEdge;
+                }
+            }
+            edge = bestEdge;
+        }
+        if (path.length() == 0) continue;
+        std::cerr << "Found path " << path << std::endl;
+
+        *statsFile << path.length() << " " << pathWeight << std::endl;
+        //Go through the path and set the seed multiplicities
+        for (int i = 0; i < path.length() - params.k; i++) {
+            std::string seed = kmerToSeed(path.substr(i, params.k), seedPattern);
+            std::string packed = seedToPacked[seed];
+            seedProbabilities[packed] = edgeProbabilityToCollapseGraph(pathWeight, CONFIDENCE);
+        }
+        path.clear();
+        pathEdges.clear();
+    }
+    return seedProbabilities;
+
+}
 
 
 int main(int argc, char **argv) {
@@ -208,7 +190,7 @@ int main(int argc, char **argv) {
     std::string lastzFilename;
     std::string statsFilename;
     int seedMultiplicity = 0;
-    int highMultiplicityPath = 0;
+    int pathMultiplicity = 0;
     struct option longopts[] = {
         {"graphFile", required_argument, 0, 'a'},
         {"sequenceFile", required_argument, 0, 'b'},
@@ -216,13 +198,13 @@ int main(int argc, char **argv) {
         {"statsFile", required_argument, 0, 'j'},
 
         {"k", required_argument, 0, 'e'},
-        {"pathThreshold", required_argument, 0, 'f'},
+        {"minPathMultiplicity", required_argument, 0, 'f'},
         {"maxPathLength", required_argument, 0, 'g'},
         {"scoreThreshold", required_argument, 0, 'h'},
         {"seedPattern", required_argument, 0, 'i'},
 
         {"seedMultiplicity", no_argument, &seedMultiplicity, 1},
-        {"pathMultiplicity", no_argument, &highMultiplicityPath, 1},
+        {"pathMultiplicity", no_argument, &pathMultiplicity, 1},
         {0, 0, 0, 0} 
     };
     int flag;
@@ -244,7 +226,7 @@ int main(int argc, char **argv) {
                 params.k = atoi(optarg);
                 break;
             case 'f':
-                params.pathThreshold = atof(optarg);
+                params.minPathMultiplicity = atof(optarg);
                 break;
             case 'g':
                 params.maxPathLength = atoi(optarg);
@@ -261,28 +243,73 @@ int main(int argc, char **argv) {
                 break;
         }
     }
-    std::map<unsigned int, int> seedMultiplciity = parseLastzTable(lastzFilename);
-    SeedProbabilities seedProbabilities;
+    std::ofstream *statsFile = NULL;
+    if (statsFilename.length() > 0) {
+        statsFile = new std::ofstream(statsFilename);
+    }
+
+    std::map<std::string, int> seedToMultiplicity;
+    std::map<std::string, std::string> seedToPacked;
+
+    //Parse the seed counts from lastz
+    std::string seed;
+    std::string packed;
+    unsigned int count;
+
+    std::ifstream lastzFile(lastzFilename);
+    std::string line;
+    const std::regex pattern("([0-9A-F]+)\\/([ACTGx]+):\\s+([0-9]+)");
+    std::stringstream ss;
+    std::smatch match;
+
+    while (std::getline(lastzFile, line)) {
+        std::regex_match(line, match, pattern);
+        if (match.size() < 3) continue;
+        ss.clear();
+        ss << match[1];
+        ss >> packed;
+        ss.clear();
+        ss << match[2];
+        ss >> seed;
+        ss.clear();
+        ss << match[3];
+        ss >> count;
+        ss.clear();
+
+        //std::cerr << "Parsed seed " << seed << " with packed representation " 
+        //    << packed << std::endl;
+        seedToPacked[seed] = packed;
+        seedToMultiplicity[packed] = count;
+    }
+
+    std::map<std::string, double> seedProbabilities;
     if (seedMultiplicity) {
-        seedProbabilities = seedProbabilitiesFromSeedMultiplicity(seedMultiplicity);
-    }
-    else if (highMultiplicityPath) {
-        CompactedDeBruijn graph(sequenceFilename, graphFilename, params);
-        seedProbabilities = seedProbabilitiesFromPathMultiplicity(seedMultiplicity, graph);
-    }
+        for (auto &kv : seedToMultiplicity) {
+            std::string packed = kv.first;
+            double multiplicity = (double)kv.second;
+            if (multiplicity > 1.0) {
+                seedProbabilities[packed] = edgeProbabilityToCollapseGraph(multiplicity, CONFIDENCE);
+            }
+            else {
+                seedProbabilities[packed] = 1.0;
+            }
 
-
-    //Get the packed representation of each seed from Lastz's position table for now
-    std::map<std::string, unsigned long> seedToPacked = parseLastzTable(lastzFilename);
-
-
-	for(auto& kv : seedToPacked) {
-        std::string seed = kv.first;
-        unsigned long packed = kv.second;
-        double probability = 1.0;
-        if (seedProbabilities.count(seed)) {
-            probability = seedProbabilities[seed];
         }
-        printf("%lx %f\n", packed, probability);
+            
+    }
+    else if (pathMultiplicity) {
+        std::vector<std::string> genomes;
+        genomes.push_back(sequenceFilename);
+        CompactedDeBruijn graph(graphFilename, genomes, params, statsFile);
+        seedProbabilities = graph.findHighMultiplicityPaths(params.seedPattern, seedToPacked);
+    }
+
+	for(auto& kv : seedProbabilities) {
+        std::string packed = kv.first;
+        double probability = kv.second;
+        printf("%s %lf\n", packed.c_str(), probability);
+    }
+    if (statsFile) {
+        statsFile->close();
     }
 }
