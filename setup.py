4fd65cdeadf96b597c8c7e28224bf72d5da0a88c
Alden <adderan@ucsc.edu>
2016-11-21 16:30:21 -0800

    Fix installation with "setup.py install" [ci skip]

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ os.system("pip install git+https://github.com/ComparativeGenomicsToolkit/sonLib@
 versionFile = "src/cactus/shared/version.py"
 if os.path.exists(versionFile):
     os.remove(versionFile)
-git_commit = subprocess.check_output("cd %s && git rev-parse HEAD" % os.path.dirname(__file__), shell=True).rstrip().strip()
+git_commit = subprocess.check_output('git log --pretty=oneline -n 1 -- $(pwd)', shell=True).split()[0]
 with open(versionFile, 'w') as versionFH:
     versionFH.write("cactus_commit = '%s'" % git_commit)
 
