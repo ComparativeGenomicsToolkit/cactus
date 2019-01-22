a63304f90d871da63f607c6283e7bd61ab6c53d7
Joel Armstrong <jcarmstr@ucsc.edu>
2019-01-22 15:08:13 -0800

    Add functionality to filter out crummy overaligned contigs

--- src/cactus/blast/mappingQualityRescoringAndFiltering.py
+++ src/cactus/blast/mappingQualityRescoringAndFiltering.py
@@ -24,11 +24,65 @@ filtering the alignments to remove lower probability alignments.
         for example to only keep the primary alignment: C subscript: cactus_calculateMappingQualities
 
 """
+from collections import defaultdict
+
 from cactus.shared.common import cactus_call
 
 def countLines(inputFile):
     with open(inputFile, 'r') as f:
         return sum(1 for line in f)
+
+def findJunkContigs(inputAlignmentPath, maxAverageOverlap=100):
+    """
+    Find a set of overaligned contigs that should be filtered.
+
+    See filterJunkContigs description below.
+    """
+    minSize = defaultdict(int)
+    totalOverlap = defaultdict(int)
+    with open(inputAlignmentPath) as f:
+        for line in f:
+            fields = line.split()
+            name1 = fields[1]
+            start1 = int(fields[2])
+            end1 = int(fields[3])
+            minSize[name1] = max((start1, end1, minSize[name1]))
+            totalOverlap[name1] += abs(end1 - start1)
+            name2 = fields[5]
+            start2 = int(fields[6])
+            end2 = int(fields[7])
+            minSize[name2] = max((start2, end2, minSize[name2]))
+            totalOverlap[name2] += abs(end2 - start2)
+    contigsToFilter = set()
+    for name in minSize:
+        size = minSize[name]
+        overlap = totalOverlap[name]
+        if float(overlap) / size > maxAverageOverlap:
+            contigsToFilter.add(name)
+    return contigsToFilter
+
+def filterJunkContigs(inputAlignmentPath, outputAlignmentPath):
+    """
+    Remove small alignments to/from contigs that appear to be aligning
+    to far too many places.
+
+    Some assemblers (*cough* DISCOVAR *cough*) sometimes barf on highly
+    repetitive regions and just output hundreds of read-length "contigs"
+    containing identical sequence. This causes massive problems downstream
+    when splitting on alignment overlaps, because we will have hundreds of
+    alignments per site creating many millions of useless alignments
+    total.
+    """
+    contigsToFilter = findJunkContigs(inputAlignmentPath)
+    with open(inputAlignmentPath) as infile, open(outputAlignmentPath, 'w') as outfile:
+        for line in infile:
+            fields = line.split()
+            name1 = fields[1]
+            name2 = fields[5]
+            if name1 in contigsToFilter or name2 in contigsToFilter:
+                continue
+            else:
+                outfile.write(line)
 
 def mappingQualityRescoring(job, inputAlignmentFileID, 
                             minimumMapQValue, maxAlignmentsPerSite, alpha, logLevel):
@@ -37,7 +91,10 @@ def mappingQualityRescoring(job, inputAlignmentFileID,
     
     Returns primary alignments and secondary alignments in two separate files.
     """
-    inputAlignmentFile = job.fileStore.readGlobalFile(inputAlignmentFileID)
+    originalAlignmentFile = job.fileStore.readGlobalFile(inputAlignmentFileID)
+
+    inputAlignmentFile = job.fileStore.getLocalTempFile()
+    filterJunkContigs(originalAlignmentFile, inputAlignmentFile)
     
     job.fileStore.logToMaster("Input cigar file has %s lines" % countLines(inputAlignmentFile))
     
