36c11f6ef52d72a4de5aad5e02a28f2838200f5e
Mobin Asri <masri@ucsc.edu>
2020-05-02 20:07:37 -0700

    Modified server service to work with changes made in server control module
    The name "kt" is replaced by "db" since this service is no more just for Kyoto Tycoon and should be able to run and maintain any database.
    Instead of importing the functions, the class of KtServer is imported that contains all the functionalities in itself.
    A function is added for getting the right server object (is going to be necessary after adding other databases)

--- src/cactus/pipeline/dbServerToil.py
+++ src/cactus/pipeline/dbServerToil.py
@@ -7,10 +7,9 @@
 import os
 import stat
 from toil.job import Job
-from cactus.pipeline.ktserverControl import runKtserver, blockUntilKtserverIsRunning, stopKtserver, \
-    blockUntilKtserverIsFinished
+from cactus.pipeline.ktserverControl import KtServer
 
-class KtServerService(Job.Service):
+class DbServerService(Job.Service):
     def __init__(self, dbElem, isSecondary, existingSnapshotID=None,
                  memory=None, cores=None, disk=None):
         Job.Service.__init__(self, memory=memory, cores=cores, disk=disk, preemptable=False)
@@ -19,6 +18,7 @@ class KtServerService(Job.Service):
         self.existingSnapshotID = existingSnapshotID
         self.failed = False
         self.process = None
+        self.dbServer = None
 
     def start(self, job):
         snapshotExportID = job.fileStore.jobStore.getEmptyFileStoreID()
@@ -28,23 +28,24 @@ class KtServerService(Job.Service):
         # need to write something to it, obviously that won't do.
         path = job.fileStore.readGlobalFile(snapshotExportID)
         os.chmod(path, stat.S_IRUSR | stat.S_IWUSR | stat.S_IRGRP | stat.S_IWGRP | stat.S_IROTH)
-        self.process, self.dbElem, self.logPath = runKtserver(self.dbElem, fileStore=job.fileStore,
-                                                              existingSnapshotID=self.existingSnapshotID,
-                                                              snapshotExportID=snapshotExportID)
+        self.dbServer = getDbServer(self.dbElem, fileStore=job.fileStore,
+                                    existingSnapshotID=self.existingSnapshotID,
+                                    snapshotExportID=snapshotExportID)
+        self.process, self.dbElem, self.logPath = self.dbServer.runServer()
         assert self.dbElem.getDbHost() != None
-        blockUntilKtserverIsRunning(self.logPath)
+        self.dbServer.blockUntilServerIsRunning()
         self.check()
         return self.dbElem.getConfString(), snapshotExportID
 
     def stop(self, job):
         self.check()
         try:
-            stopKtserver(self.dbElem)
+            self.dbServer.stopServer()
         except:
             # Server is probably already terminated
             pass
         if not self.failed:
-            blockUntilKtserverIsFinished(self.logPath, timeout=1200)
+            self.dbServer.blockUntilServerIsFinished()
 
     def check(self):
         if self.process.exceptionMsg.empty():
@@ -53,3 +54,13 @@ class KtServerService(Job.Service):
             self.failed = True
             msg = self.process.exceptionMsg.get()
             raise RuntimeError(msg)
+
+
+def getDbServer(dbElem, fileStore=None, existingSnapshotID=None, snapshotExportID=None):
+    """
+    Get the correct object that handles the database server based on database type
+    Each database has a specific class with common functionalities
+    """
+    if dbElem.getDbType() == "kyoto_tycoon":
+        return KtServer(dbElem, fileStore, existingSnapshotID, snapshotExportID)
+    raise RuntimeError("The database type, %s, is not supported" % dbElem.getDbType())
