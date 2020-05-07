5670116f140f85ac39b74d3e1891d4e5a866d974
Mobin Asri <masri@ucsc.edu>
2020-05-06 19:45:30 -0700

    Modified the redis server module to work with redis commands and options
    'ktremotemgr' and 'ktserver' commands are replaced by 'redis-cli' and 'redis-server' respectively. The keywords that are being searched in the log file are updated to work with redis log file. The options for running the server are updated. There is no need to check the existence of two snapshots. Some TODOs are added.

--- src/cactus/pipeline/redisServerControl.py
+++ src/cactus/pipeline/redisServerControl.py
@@ -8,7 +8,6 @@ import random
 import signal
 import sys
 import traceback
-from glob import glob
 from multiprocessing import Process, Queue
 from time import sleep
 
@@ -16,11 +15,11 @@ from toil.lib.bioio import logger
 from cactus.shared.common import cactus_call
 from cactus.pipeline.dbServerCommon import getHostName, findOccupiedPorts
 
-# For some reason ktserver believes there are only 32768 TCP ports.
-MAX_KTSERVER_PORT = 32767
 
-# The name of the snapshot that KT outputs.
-KTSERVER_SNAPSHOT_NAME = "00000000.ktss"
+MAX_REDIS_PORT = 65535
+
+# The name of the snapshot that Redis outputs.
+REDIS_SNAPSHOT_NAME = "dump.rdb"
 
 
 class RedisServer:
@@ -30,10 +29,11 @@ class RedisServer:
         self.fileStore = fileStore
         self.existingSnapshotID = existingSnapshotID
         self.snapshotExportID = snapshotExportID
+        self.databaseDir = None
 
     def runServer(self):
         """
-        Run a KTServer. This function launches a separate python process that manages the server.
+        Run a redis-server. This function launches a separate python process that manages the server.
 
         Writing to the special key "TERMINATE" signals this thread to safely shut
         down the DB and save the results. After finishing, the data will
@@ -42,17 +42,22 @@ class RedisServer:
         Returns a tuple containing an updated version of the database config dbElem and the
         path to the log file.
         """
-        self.logPath = self.fileStore.getLocalTempFile()
+
+        self.databaseDir = self.fileStore.getLocalTempDir()
+        # log file can be saved in a subdirectory of where the snapshot is being saved
+        self.logPath = os.path.join(self.databaseDir, "redis.log")
+        open(self.logPath, 'a').close()
+
         self.dbElem.setDbHost(getHostName())
         # Find a suitable port to run on.
         try:
             occupiedPorts = findOccupiedPorts()
-            unoccupiedPorts = set(range(1025, MAX_KTSERVER_PORT)) - occupiedPorts
+            unoccupiedPorts = set(range(1025, MAX_REDIS_PORT)) - occupiedPorts
             port = random.choice(list(unoccupiedPorts))
         except:
             logger.warning("Can't find which ports are occupied--likely netstat is not installed."
                            " Choosing a random port to start the DB on, good luck!")
-            port = random.randint(1025,MAX_KTSERVER_PORT)
+            port = random.randint(1025, MAX_REDIS_PORT)
         self.dbElem.setDbPort(port)
         process = RedisServerProcess(self)
         process.daemon = True
@@ -64,45 +69,45 @@ class RedisServer:
                     log = f.read()
             except:
                 log = ''
-            raise RuntimeError("Unable to launch ktserver in time. Log: %s" % log)
+            raise RuntimeError("Unable to launch redis-server in time. Log: %s" % log)
 
         return process, self.dbElem, self.logPath
 
     def blockUntilServerIsRunning(self, createTimeout=1800):
         """Check status until it's successful, an error is found, or we timeout.
 
-        Returns True if the ktserver is now running, False if something went wrong."""
+        Returns True if the redis-server is now running, False if something went wrong."""
         success = False
         for i in range(createTimeout):
             if self.isServerFailed():
-                logger.critical('Error starting ktserver.')
+                logger.critical('Error starting Redis server.')
                 success = False
                 break
             if self.isServerRunning():
-                logger.info('Ktserver running.')
+                logger.info('Redis server running.')
                 success = True
                 break
             sleep(1)
         return success
 
     def blockUntilServerIsFinished(self, timeout=1800, timeStep=10):
-        """Wait for the ktserver log to indicate that it shut down properly.
+        """Wait for the redis-server log to indicate that it shut down properly.
 
         Returns True if the server shut down, False if the timeout expired."""
         for i in range(0, timeout, timeStep):
             with open(self.logPath) as f:
                 log = f.read()
-                if '[FINISH]' in log:
+                if 'ready to exit' in log:
                     return True
             sleep(timeStep)
-        raise RuntimeError("Timeout reached while waiting for ktserver.")
+        raise RuntimeError("Timeout reached while waiting for redis server.")
 
     def isServerRunning(self):
         """Check if the server started running."""
         success = False
         with open(self.logPath) as f:
             for line in f:
-                if line.lower().find("listening") >= 0:
+                if line.lower().find("accept connections") >= 0:
                     success = True
         return success
 
@@ -117,10 +122,11 @@ class RedisServer:
         return isFailed
 
     def getTuningOptions(self):
-        """Get the appropriate KTServer tuning parameters (bucket size, etc.)"""
+        """Get the appropriate redis-server tuning parameters (bucket size, etc.)"""
         # these are some hardcoded defaults.  should think about moving to config
-        tuningOptions = "#opts=ls#bnum=30m#msiz=50g#ktopts=p"
-        # override default ktserver settings if they are present in the
+        # TODO: check if every necessary tuning option is added (maybe to be merged with getServerOptions())
+        tuningOptions = "--maxmemory 50Gb"
+        # override default redis-server settings if they are present in the
         # experiment xml file.
         if self.dbElem.getDbTuningOptions() is not None:
             tuningOptions = self.dbElem.getDbTuningOptions()
@@ -130,38 +136,41 @@ class RedisServer:
 
     def getServerOptions(self):
         # these are some hardcoded defaults.  should think about moving to config
-        serverOptions = "-ls -tout 200000 -th 64"
+        # TODO: check if every necessary option is added
+        serverOptions = "--timeout 0 --databases 1"
         if self.dbElem.getDbServerOptions() is not None:
             serverOptions = self.dbElem.getDbServerOptions()
         return serverOptions
 
     def getServerCommand(self, snapshotDir):
-        """Get a ktserver command line with the proper options (in popen-type list format)."""
+        """Get a redis-server command line with the proper options (in popen-type list format)."""
         serverOptions = self.getServerOptions()
         tuning = self.getTuningOptions()
-        cmd = ["ktserver", "-port", str(self.dbElem.getDbPort())]
+        cmd = ["redis-server", "--port", str(self.dbElem.getDbPort())]
         cmd += serverOptions.split()
         # Configure background snapshots, but set the interval between
         # snapshots to ~ 10 days so it'll never trigger. We are only
         # interested in the snapshot that the DB creates on termination.
-        cmd += ["-bgs", snapshotDir, "-bgsc", "lzo", "-bgsi", "1000000"]
-        cmd += ["-log", self.logPath]
-        cmd += [":" + tuning]
+        cmd += ["--save", "1000000", "1"]
+        cmd += ["--dir", snapshotDir, "--dbfilename", REDIS_SNAPSHOT_NAME]
+        cmd += ["--logfile", 'redis.log']
+        cmd += tuning.split()
         return cmd
 
     def getRemoteParams(self):
-        """Get parameters to supply to ktremotemgr to connect to the right DB."""
-        host = self.dbElem.getDbHost() or 'localhost'
-        return ['-port', str(self.dbElem.getDbPort()),
-                '-host', host]
+        """Get parameters to supply to redis-cli to connect to the right DB."""
+        # TODO: find why redis-cli raise error when called with an IP as the host
+        #host = self.dbElem.getDbHost() or 'localhost'
+        host = 'localhost'
+        return ['-p', str(self.dbElem.getDbPort()), '-h', host]
 
     def stopServer(self):
-        """Attempt to send the terminate signal to a ktserver."""
-        cactus_call(parameters=['ktremotemgr', 'set'] + self.getRemoteParams() + ['TERMINATE', '1'])
+        """Attempt to send the terminate signal to a redis-server."""
+        cactus_call(parameters=['redis-cli'] + self.getRemoteParams() + ['set', 'TERMINATE', '1'])
 
 
 class RedisServerProcess(Process):
-    """Independent process that babysits the ktserver process.
+    """Independent process that babysits the redis-server process.
 
     Waits for the TERMINATE flag to be set, then kills the DB and
     copies the final snapshot to snapshotExportID.
@@ -181,51 +190,43 @@ class RedisServerProcess(Process):
             self.exceptionMsg.put("".join(traceback.format_exception(*sys.exc_info())))
             raise
 
-    def tryRun(self, ktServer):
-        snapshotDir = os.path.join(ktServer.fileStore.getLocalTempDir(), 'snapshot')
-        os.mkdir(snapshotDir)
-        snapshotPath = os.path.join(snapshotDir, KTSERVER_SNAPSHOT_NAME)
-        if ktServer.existingSnapshotID is not None:
+    def tryRun(self, redisServer):
+        snapshotDir = redisServer.databaseDir
+        snapshotPath = os.path.join(snapshotDir, REDIS_SNAPSHOT_NAME)
+        if redisServer.existingSnapshotID is not None:
             # Extract the existing snapshot to the snapshot
             # directory so it will be automatically loaded
-            ktServer.fileStore.readGlobalFile(ktServer.existingSnapshotID, userPath=snapshotPath)
+            redisServer.fileStore.readGlobalFile(redisServer.existingSnapshotID, userPath=snapshotPath)
         process = cactus_call(server=True, shell=False,
-                              parameters=ktServer.getServerCommand(snapshotDir),
-                              port=ktServer.dbElem.getDbPort())
-        ktServer.blockUntilServerIsRunning()
-        if ktServer.existingSnapshotID is not None:
+                              parameters=redisServer.getServerCommand(snapshotDir),
+                              port=redisServer.dbElem.getDbPort())
+
+        redisServer.blockUntilServerIsRunning()
+        if redisServer.existingSnapshotID is not None:
             # Clear the termination flag from the snapshot
-            cactus_call(parameters=["ktremotemgr", "remove"] + ktServer.getRemoteParams() + ["TERMINATE"])
+            cactus_call(parameters=["redis-cli"] + redisServer.getRemoteParams() + ["del", "TERMINATE"])
         while True:
             # Check for the termination signal
-            try:
-                cactus_call(parameters=["ktremotemgr", "get"] + ktServer.getRemoteParams() + ["TERMINATE"],
-                            swallowStdErr=True)
-            except:
+            terminateFlag = cactus_call(parameters=["redis-cli"] + redisServer.getRemoteParams() + ["get", "TERMINATE"],
+                                        swallowStdErr=True, check_output=True)
+            if terminateFlag.strip() != '1':
                 # No terminate signal sent yet
                 pass
             else:
                 # Terminate signal received
                 break
             # Check that the DB is still alive
-            if process.poll() is not None or ktServer.isServerFailed():
-                with open(ktServer.logPath) as f:
-                    raise RuntimeError("KTServer failed. Log: %s" % f.read())
+            if process.poll() is not None or redisServer.isServerFailed():
+                with open(redisServer.logPath) as f:
+                    raise RuntimeError("redis server failed. Log: %s" % f.read())
             sleep(60)
         process.send_signal(signal.SIGINT)
         process.wait()
-        ktServer.blockUntilServerIsFinished()
-        if ktServer.snapshotExportID is not None:
+        redisServer.blockUntilServerIsFinished()
+        if redisServer.snapshotExportID is not None:
             if not os.path.exists(snapshotPath):
-                with open(ktServer.logPath) as f:
-                    raise RuntimeError("KTServer did not leave a snapshot on termination,"
+                with open(redisServer.logPath) as f:
+                    raise RuntimeError("redis-server did not leave a snapshot on termination,"
                                        " but a snapshot was requested. Log: %s" % f.read())
-            if len(glob(os.path.join(snapshotDir, "*.ktss"))) != 1:
-                # More than one snapshot file. It's not clear what
-                # conditions trigger this--if any--but we
-                # don't support it right now.
-                with open(ktServer.logPath) as f:
-                    raise RuntimeError("KTServer left more than one snapshot. Log: %s" % f.read())
-
             # Export the snapshot file to the file store
-            ktServer.fileStore.jobStore.updateFile(ktServer.snapshotExportID, snapshotPath)
+            redisServer.fileStore.jobStore.updateFile(redisServer.snapshotExportID, snapshotPath)
