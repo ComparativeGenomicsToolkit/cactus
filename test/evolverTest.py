30754a91e0dd688d0b76f2c3def497f352dbcc0a
Glenn Hickey <glenn.hickey@gmail.com>
2020-10-18 15:41:38 -0400

    typo

--- test/evolverTest.py
+++ test/evolverTest.py
@@ -309,7 +309,7 @@ class TestCase(unittest.TestCase):
         # check the output
         self._check_stats(self._out_hal("wdl"), delta_pct=0.25)
         self._check_coverage(self._out_hal("wdl"), delta_pct=0.20)
-        self._check_maf_accuracy(self._out_hal(name), delta=0.01)
+        self._check_maf_accuracy(self._out_hal("wdl"), delta=0.01)
 
     def testEvolverPrepareToil(self):
 
