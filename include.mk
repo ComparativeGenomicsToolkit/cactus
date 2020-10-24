c266ee52a2f749ef60c0cce394a83167abb08886
masri2019 <masri@ucsc.edu>
2020-10-24 05:30:00 +0000

    Updated hiredis CFLAGs since no more built as a submodule

--- include.mk
+++ include.mk
@@ -46,8 +46,8 @@ inclDirs = api/inc bar/inc caf/inc hal/inc reference/inc submodules/sonLib/C/inc
 
 kyotoTycoonIncl=-I${rootPath}/include -DHAVE_KYOTO_TYCOON=1
 kyotoTycoonLib=-L${rootPath}/lib -Wl,-rpath,${rootPath}/lib -lkyototycoon -lkyotocabinet -lz -lbz2 -lpthread -lm -lstdc++
-hiredisIncl=-I${rootPath}/include/hiredis -DHAVE_REDIS=1
-hiredisLib=-L${rootPath}/lib -lhiredis
+hiredisIncl=$(shell pkg-config --cflags hiredis) -DHAVE_REDIS=1
+hiredisLib=-lhiredis
 
 CPPFLAGS += ${inclDirs:%=-I${rootPath}/%} -I${LIBDIR} ${kyotoTycoonIncl}
 
