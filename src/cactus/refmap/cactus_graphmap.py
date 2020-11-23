2a43c1637369d522df669d3cd75e20ad832d0cb0
Glenn Hickey <glenn.hickey@gmail.com>
2020-11-23 18:17:28 -0500

    add gaf node length filter

--- src/cactus/refmap/cactus_graphmap.py
+++ src/cactus/refmap/cactus_graphmap.py
@@ -268,6 +268,9 @@ def merge_gafs_into_paf(job, config, gaf_file_ids):
     gaf_block = getOptionalAttrib(xml_node, "minGAFBlockLength", int)
     if gaf_block:
         mzgaf2paf_opts += ['-b', str(gaf_block)]
+    gaf_node = getOptionalAttrib(xml_node, "minGAFNodeLength", int)
+    if gaf_node:
+        mzgaf2paf_opts += ['-s', str(gaf_node)]        
 
     cactus_call(work_dir=work_dir, outfile=paf_path, parameters=["mzgaf2paf"] + gaf_paths + mzgaf2paf_opts)
 
