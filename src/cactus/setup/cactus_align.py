29b5e96493e8015868dfdd974cf43ffa61e8567a
Glenn Hickey <glenn.hickey@gmail.com>
2021-08-04 09:29:09 -0400

    make default recoverable chains be poa window length all the time

--- src/cactus/setup/cactus_align.py
+++ src/cactus/setup/cactus_align.py
@@ -399,8 +399,6 @@ def make_align_job(options, toil):
         cafNode.attrib["runMapQFiltering"] = "0"
         # more iterations here helps quite a bit to reduce underalignment
         cafNode.attrib["maxRecoverableChainsIterations"] = "50"
-        # pulling apart a recoverable chain bigger than the poa window is counterproductive
-        cafNode.attrib["maxRecoverableChainLength"] = "10000"
         # turn down minimum block degree to get a fat ancestor
         barNode.attrib["minimumBlockDegree"] = "1"
         # turn on POA
