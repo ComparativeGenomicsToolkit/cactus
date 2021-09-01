e3d830e53e5320b1614117754f27c092bb338a13
benedictpaten <benedictpaten@gmail.com>
2021-09-01 13:47:13 -0700

    Adding scripts to automate the creation of the chained alignments for the ingroup and outgroup genomes.

--- paf/cactus_lastz_alignment.py
+++ paf/cactus_lastz_alignment.py
@@ -11,6 +11,7 @@ Released under the MIT license, see LICENSE.txt
 from toil.common import Toil
 from toil.job import Job
 from toil.lib.bioio import system
+from cactus.shared.common import makeURL
 
 def run_lastz(job, genome_A, genome_B, distance, params):
     # Create a local temporary file to put the alignments in.
@@ -20,36 +21,49 @@ def run_lastz(job, genome_A, genome_B, distance, params):
     ##
 
     # Generate the alignment
-    system("lastz {} {} --cigar=format:paf > {}".format(genome_A, genome_B, alignment_file))
+    system("lastz {} {} --format=paf:minimap2 > {}".format(job.fileStore.readGlobalFile(genome_A),
+                                                           job.fileStore.readGlobalFile(genome_B),
+                                                           alignment_file))
 
     # Return the alignment file
     return job.fileStore.writeGlobalFile(alignment_file)
 
-def combine_alignments(job, alignments_A_B, alignments_A_C, alignments_B_C, output_alignments):
-    # The chaining process
-    system("cactus_chains {} {} {} {}".format(alignments_A_B, alignments_A_C, alignments_B_C, output_alignments))
+def chain_alignments(job, alignments_A_B, alignments_A_C, alignments_B_C):
+    # Create a local temporary file to put the alignments in.
+    output_alignments_file = job.fileStore.getLocalTempFile()
 
-def run_lastz_alignments(job, genome_A, genome_B, genome_C, output_alignments_file, params,
+    # Run the chaining
+    system("cactus_chain {} {} {} {}".format(job.fileStore.readGlobalFile(alignments_A_B),
+                                              job.fileStore.readGlobalFile(alignments_A_C),
+                                              job.fileStore.readGlobalFile(alignments_B_C),
+                                              output_alignments_file))
+
+    return job.fileStore.writeGlobalFile(output_alignments_file)
+
+def run_lastz_alignments(job, genome_A, genome_B, genome_C, params,
                          distance_A_B=1.0, distance_A_C=1.0, distance_B_C=1.0):
     # Generate the alignments
-    alignments_A_B = job.addChildJobFn(run_lastz, genome_A, genome_B, distance_A_B, params) # Generate alignments A to B
-    alignments_A_C = job.addChildJobFn(run_lastz, genome_A, genome_C, distance_A_C, params) # Generate alignments A to C
-    alignments_B_C = job.addChildJobFn(run_lastz, genome_B, genome_C, distance_B_C, params) # Generate alignments B to C
+    alignments_A_B = job.addChildJobFn(run_lastz, genome_A, genome_B, distance_A_B, params).rv()  # Generate alignments A to B
+    alignments_A_C = job.addChildJobFn(run_lastz, genome_A, genome_C, distance_A_C, params).rv()  # Generate alignments A to C
+    alignments_B_C = job.addChildJobFn(run_lastz, genome_B, genome_C, distance_B_C, params).rv()  # Generate alignments B to C
     # Now do the chaining
-    job.addFollowOnJobFn(combine_alignments, alignments_A_B, alignments_A_C, alignments_B_C,
-                         output_alignments_file)
+    return job.addFollowOnJobFn(chain_alignments, alignments_A_B, alignments_A_C, alignments_B_C).rv()
 
 if __name__ == "__main__":
     parser = Job.Runner.getDefaultArgumentParser()
 
-    parser.add_argument("genome_A", help = "Ingroup genomes A file [fasta]")
-    parser.add_argument("genome_B", help = "Ingroup genomes B file [fasta]")
-    parser.add_argument("genome_C", help = "Outgroup genomes C file [fasta]")
-    parser.add_argument("output_file", help = "Output alignments file [paf]")
+    parser.add_argument("genome_A", help="Ingroup genomes A file [fasta]")
+    parser.add_argument("genome_B", help="Ingroup genomes B file [fasta]")
+    parser.add_argument("genome_C", help="Outgroup genomes C file [fasta]")
+    parser.add_argument("output_file", help="Output alignments file [paf]")
+    parser.add_argument("--params", help="Cactus parameters file")
 
     options = parser.parse_args()
     options.clean = "always"
     with Toil(options) as toil:
-        toil.start(Job.wrapJobFn(run_lastz_alignments,
-                                 options.genome_A, options.genome_B, options.genome_C,
-                                 options.output_file, options))
+        output_file_ID = toil.start(Job.wrapJobFn(run_lastz_alignments,
+                                    toil.importFile(makeURL(options.genome_A)),
+                                    toil.importFile(makeURL(options.genome_B)),
+                                    toil.importFile(makeURL(options.genome_C)),
+                                    options))
+        toil.exportFile(output_file_ID, makeURL(options.output_file))
