ed9e2e55ccdfdbcafc60b38fa54b2b5f63969a3c
Glenn Hickey <glenn.hickey@gmail.com>
2021-12-09 16:47:44 -0500

    remove --nonCactusInput option from test

--- test/evolverTest.py
+++ test/evolverTest.py
@@ -161,7 +161,6 @@ class TestCase(unittest.TestCase):
                 if line.startswith('cactus-align'):
                     #Remove all the id prefixes to pretend the cigars came not cactus-blast
                     subprocess.check_call('sed -i -e \'s/id=[0,1]|//g\' {}/Anc0.cigar*'.format(out_dir), shell=True)
-                    line += ' --nonCactusInput'
                 sys.stderr.write('Running {}'.format(line))
                 subprocess.check_call(line, shell=True)
 
@@ -458,7 +457,7 @@ class TestCase(unittest.TestCase):
 
     def testEvolverPrepareNoOutgroupDocker(self):
 
-        # run cactus step by step via the plan made by cactus-prepare, hacking to apply --nonCactusInput option to cactus-align
+        # run cactus step by step via the plan made by cactus-prepare
         self._run_evolver_decomposed_no_outgroup("docker")
 
         # check the output
@@ -467,7 +466,7 @@ class TestCase(unittest.TestCase):
 
     def testEvolverPrepareNoOutgroupLocal(self):
 
-        # run cactus step by step via the plan made by cactus-prepare, hacking to apply --nonCactusInput option to cactus-align
+        # run cactus step by step via the plan made by cactus-prepare
         self._run_evolver_decomposed_no_outgroup("local")
 
         # check the output
