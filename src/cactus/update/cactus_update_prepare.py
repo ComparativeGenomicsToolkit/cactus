f6613cd60a28a7ee69aed1ea1e2cfe5084bef576
Thiago Genez <thiagogenez@ebi.ac.uk>
2022-01-27 18:33:12 +0000

    refactoring

--- src/cactus/update/cactus_update_prepare.py
+++ src/cactus/update/cactus_update_prepare.py
@@ -1,36 +1,24 @@
+
+# Native library
+from os import path
 from io import StringIO
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 
+# Cactus library
 from cactus.shared.common import cactus_call
 
-# manipulate Newick-format tree
+# Sonlib library
 from sonLib.nxnewick import NXNewick
 from sonLib.nxtree import NXTree
 
+def call_cactus_prepare():
+    return
 
-# def halStats_call_get_children(hal_file, genome):
-#    """Specific call function to get the genome's children using the halStats tool"""
-#    children = cactus_call(check_output=True, parameters=[
-#                           "halStats", "--children", genome, hal_file])
-#    return children.split()
-
-
-def halStats_call_get_tree(hal_file):
-    """Specific call function to get the tree of a HAL-format file using the halStats tool"""
-    return cactus_call(check_output=True, parameters=["halStats", "--tree", hal_file])
-
-
-def hal2fasta_call_get_fasta(hal_file, fasta_file, genome, options="--hdf5InMemory"):
-    """Specific call function to transform a HAL-format file into a FASTA-format file using the hal2fasta tool"""
-    cactus_call(parameters=["hal2fasta", hal_file,
-                genome] + options.split() + ["> ", fasta_file])
-
-
-def extract_newick_tree(hal_file):
-    """Extract the newick tree from a HAL-format file
+def extract_newick_tree(hal_filename):
+    """Extract the newick tree from a HAL-format file using the halStats binary tool
 
     Args:
-        hal_file (str): The path for the HAL-format file
+        hal_filename (str): The path for the HAL-format file
 
     Raises:
         RuntimeError: Failed to parse the newick tree extract from the HAL-format file
@@ -40,7 +28,8 @@ def extract_newick_tree(hal_file):
     """
 
     # extract the tree in a string format
-    string_tree = halStats_call_get_tree(hal_file)
+    string_tree = cactus_call(check_output=True, parameters=[
+                              'halStats', '-tree', hal_filename])
 
     # parse and sanity check
     newickParser = NXNewick()
@@ -52,6 +41,34 @@ def extract_newick_tree(hal_file):
 
     return tree
 
+
+def extract_fasta_files(hal_filename, genomes, hal_options, output_dir):
+    """Function to extract a genome in FASTA-format from a HAL-format file using the hal2fasta binary tool
+
+    Args:
+        hal_filename (str): The path of the HAL-format file
+        genomes (List[str]): List of genomes to be extracted from the HAL-format file
+        hal_options (str): The options for customising customise the extraction 
+        output_dir (str): The directory path where the FASTA-file will be stored
+
+    Returns:
+        [Dict[str,str]]: A dict containing the filename and path for each extracted FASTA-format file
+    Raises:
+        RuntimeError: [description]
+    """
+    fasta_paths = {}
+
+    for genome in genomes:
+        fasta_filename = path.join(output_dir, genome + '.fa')
+
+        return_code = cactus_call(check_result=True, checkparameters=[
+                                  "hal2fasta", hal_filename + hal_options.split(), + ["> ",  fasta_filename]])
+        if return_code:
+            raise RuntimeError("hal exception caught: Genome {} not found".format(genome))
+        
+        fasta_paths[genome] = fasta_filename
+
+    return fasta_paths
 
 def get_node_id(tree, node_name):
     """Get the internal node ID in the tree
@@ -67,37 +84,30 @@ def get_node_id(tree, node_name):
     for node_id in tree.breadthFirstTraversal():
         if tree.getName(node_id) == node_name:
             return node_id
-    
+
     return None
 
 
-def adding2node_prepare(hal_file, target_genome_name, new_children):
-    """Adding new genomes to a target genome using an existing alingment given by a HAL-format file"""
+def get_tree_patch_adding2node(tree, target_genome_id, new_children):
+    """[summary]
 
-    # get the tree
-    tree = extract_newick_tree(hal_file)
+    Args:
+        tree ([type]): [description]
+        target_genome_id ([type]): [description]
+        new_children ([type]): [description]
 
-    # creating the seqFile as the "tree-patch" for the new alignment due to this update to a node
-    # Exemple:
-    #   - target_genome_name = genome_3
-    #   - new_children = [genome_4]
-    #   - string-format patch tree: (genome_1, genome_2, genome_4):genome_3;
+    Returns:
+        [type]: [description]
+    """
 
-
-    # get node_id of the target_genome_name
-    target_genome_id = get_node_id(tree, target_genome_name)
-    if target_genome_id is None:
-        raise RuntimeError(
-            "Genome \'{}\'  not found in the tree extract from the HAL-format file {}}".format(target_genome_name, hal_file))    
-
-
-    # open the tree patch to add the new genomes
+   # the tree patch for adding the new genomes as children of the target_genome
     patch = '('
 
     # re-adding current target_genome children to the patch
     for child_id in tree.getChildren(target_genome_id):
         child_name = tree.getName(child_id)
-        patch += '{}:{},'.format(child_name, tree.getWeight(target_genome_id, child_id))
+        patch += '{}:{},'.format(child_name,
+                                 tree.getWeight(target_genome_id, child_id))
 
     # adding the new target_getnome child(ren) to the patch
     for name, weight in new_children.items():
@@ -107,20 +117,65 @@ def adding2node_prepare(hal_file, target_genome_name, new_children):
     patch = patch.rstrip(patch[-1])
 
     # close the path and add the target_genome_name
-    patch += '){}'.format(target_genome_name)
+    patch += '){}'.format(tree.getName(target_genome_id))
     if tree.hasParent(target_genome_id):
         # target_genome is not the root of the tree
-        patch += ':{};'.format(tree.getWeight(tree.getParent(target_genome_id), target_genome_id))
+        patch += ':{};'.format(tree.getWeight(
+            tree.getParent(target_genome_id), target_genome_id))
     else:
         # target_genome is the root of the tree
         patch += ';'
 
     return patch
 
-# def adding2branch():
 
-# def cactus_update(options):
+def adding2node_prepare(target_genome_name, new_genomes, options):
+    """A funciton to prepare the addition of new genomes to a node (genome).
 
+    Args:
+        hal_filename ([type]): [description]
+        target_genome_name ([type]): [description]
+        new_genomes ([type]): [description]
+
+    Raises:
+        RuntimeError: [description]
+    """
+
+    # get the tree
+    tree = extract_newick_tree(options.halFile)
+
+    # creating the seqFile as the "tree-patch" for the new alignment due to this update to a node
+    # Exemple:
+    #   - target_genome_name = genome_3
+    #   - new_children = [genome_4]
+    #   - string-format patch tree: (genome_1, genome_2, genome_4):genome_3;
+
+    # get node_id of the target_genome_name
+    target_genome_id = get_node_id(tree, target_genome_name)
+
+    if target_genome_id is None:
+        raise RuntimeError(
+            "Genome \'{}\' not found in the tree extract from the HAL-format file {}".format(target_genome_name, options.halFile))
+
+    # get tree patch in newick format
+    patch = get_tree_patch_adding2node(tree, target_genome_id, new_genomes)
+
+    # get the target_genome's current children
+    children = [ tree.getName(i) for i in tree.getChildren(target_genome_id)]
+    fasta_filenames = extract_fasta_files(options.halFile, children, options.halOptions, options.outputDir )
+
+    # write the output
+    if options.outSeqFile:
+        with open(options.outSeqFile, 'w') as out_sf:
+            out_sf.write(patch)
+            for name, path in fasta_filenames.items():
+                out_sf.write('{}\t{}\n'.format(name, path))
+
+
+    commands = call_cactus_prepare()
+    
+    
+    
 
 def main():
     #print("children: {}".format(halStats_call_get_children("/Users/thiagogenez/Documents/buckets/adding-genome/lepidoptera/runs/0/steps/Anc05.hal", "Anc05")))
