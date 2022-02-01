5101da6cb62536babb156bf18f86f21f05463c53
Thiago Genez <thiagogenez@ebi.ac.uk>
2022-02-01 12:39:16 +0000

    fixing argparse subcommands

--- src/cactus/update/cactus_update_prepare.py
+++ src/cactus/update/cactus_update_prepare.py
@@ -1,5 +1,6 @@
 
 # Native library
+import sys
 from os import path
 from io import StringIO
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
@@ -11,8 +12,10 @@ from cactus.shared.common import cactus_call
 from sonLib.nxnewick import NXNewick
 from sonLib.nxtree import NXTree
 
+
 def call_cactus_prepare():
     return
+
 
 def extract_newick_tree(hal_filename):
     """Extract the newick tree from a HAL-format file using the halStats binary tool
@@ -42,12 +45,12 @@ def extract_newick_tree(hal_filename):
     return tree
 
 
-def extract_fasta_files(hal_filename, genomes, hal_options, output_dir):
+def extract_fasta_files(hal_file, genome_names, hal_options, output_dir):
     """Function to extract a genome in FASTA-format from a HAL-format file using the hal2fasta binary tool
 
     Args:
-        hal_filename (str): The path of the HAL-format file
-        genomes (List[str]): List of genomes to be extracted from the HAL-format file
+        hal_file (str): The path of the HAL-format file
+        genome_names (List[str]): List of names of genome to be extracted from the HAL-format file
         hal_options (str): The options for customising customise the extraction 
         output_dir (str): The directory path where the FASTA-file will be stored
 
@@ -56,19 +59,29 @@ def extract_fasta_files(hal_filename, genomes, hal_options, output_dir):
     Raises:
         RuntimeError: [description]
     """
-    fasta_paths = {}
+    fastas = {}
 
-    for genome in genomes:
-        fasta_filename = path.join(output_dir, genome + '.fa')
-
-        return_code = cactus_call(check_result=True, checkparameters=[
-                                  "hal2fasta", hal_filename + hal_options.split(), + ["> ",  fasta_filename]])
-        if return_code:
-            raise RuntimeError("hal exception caught: Genome {} not found".format(genome))
+    for genome in genome_names:
         
-        fasta_paths[genome] = fasta_filename
+        # FASTA-format filename to the correspondent genome
+        fasta_file = path.join(output_dir, genome + '.fa')
 
-    return fasta_paths
+        # create the command-line
+        # example: hal2fasta steps/Anc78.hal Anc78 --hdf5InMemory > steps/Anc78.fa
+        cmd = "hal2fasta {} {} {} > {}".format( hal_file, genome, hal_options, fasta_file)
+        
+        # call the built-in cactus_call function
+        return_code = cactus_call(check_result=True, checkparameters=cmd.split())
+        
+        # sanity check
+        if return_code:
+            raise RuntimeError(
+                "hal exception caught: Genome {} not found".format(genome))
+
+        fastas[genome] = fasta_file
+
+    return fastas
+
 
 def get_node_id(tree, node_name):
     """Get the internal node ID in the tree
@@ -88,13 +101,13 @@ def get_node_id(tree, node_name):
     return None
 
 
-def get_tree_patch_adding2node(tree, target_genome_id, new_children):
+def get_tree_patch(target_genome_name, target_genome_parent_weight, children):
     """[summary]
 
     Args:
-        tree ([type]): [description]
-        target_genome_id ([type]): [description]
-        new_children ([type]): [description]
+        target_genome_name ([type]): [description]
+        target_genome_parent_weight ([type]): [description]
+        children ([type]): [description]
 
     Returns:
         [type]: [description]
@@ -103,28 +116,23 @@ def get_tree_patch_adding2node(tree, target_genome_id, new_children):
    # the tree patch for adding the new genomes as children of the target_genome
     patch = '('
 
-    # re-adding current target_genome children to the patch
-    for child_id in tree.getChildren(target_genome_id):
-        child_name = tree.getName(child_id)
-        patch += '{}:{},'.format(child_name,
-                                 tree.getWeight(target_genome_id, child_id))
-
-    # adding the new target_getnome child(ren) to the patch
-    for name, weight in new_children.items():
+    # adding children to the patch
+    for name, weight in children.items():
         patch += '{}:{},'.format(name, weight)
 
     # remove the last ','
     patch = patch.rstrip(patch[-1])
 
     # close the path and add the target_genome_name
-    patch += '){}'.format(tree.getName(target_genome_id))
-    if tree.hasParent(target_genome_id):
+    patch += '){}'.format(target_genome_name)
+
+    # add the target_node's weight
+    if target_genome_parent_weight:
         # target_genome is not the root of the tree
-        patch += ':{};'.format(tree.getWeight(
-            tree.getParent(target_genome_id), target_genome_id))
-    else:
-        # target_genome is the root of the tree
-        patch += ';'
+        patch += ':{}'.format(target_genome_parent_weight)
+
+    # target_genome is the root of the tree
+    patch += ';'
 
     return patch
 
@@ -157,12 +165,24 @@ def adding2node_prepare(target_genome_name, new_genomes, options):
         raise RuntimeError(
             "Genome \'{}\' not found in the tree extract from the HAL-format file {}".format(target_genome_name, options.halFile))
 
-    # get tree patch in newick format
-    patch = get_tree_patch_adding2node(tree, target_genome_id, new_genomes)
+    # get current children of the target genome
+    target_children = { tree.getName(child_id): tree.getWeight(
+        target_genome_id, child_id) for child_id in tree.getChildren(target_genome_id)}
 
-    # get the target_genome's current children
-    children = [ tree.getName(i) for i in tree.getChildren(target_genome_id)]
-    fasta_filenames = extract_fasta_files(options.halFile, children, options.halOptions, options.outputDir )
+    # extract FASTA files for each target_genome's children
+    fasta_filenames = extract_fasta_files(
+        options.halFile, target_children.keys(), options.halOptions, options.outputDir)
+    
+    # update the dict with the fasta-format files of the new genomes
+    fasta_filenames.update(options.updateFile)
+
+    # add new children and get tree patch in newick format
+    target_children.update(new_genomes)
+    parent_weight = None
+    if tree.hasParent(target_genome_id):
+        weight = tree.getWeight(tree.getParent(target_genome_id), target_genome_id)
+    
+    patch = get_tree_patch(target_genome_name, parent_weight, target_children)
 
     # write the output
     if options.outSeqFile:
@@ -171,25 +191,47 @@ def adding2node_prepare(target_genome_name, new_genomes, options):
             for name, path in fasta_filenames.items():
                 out_sf.write('{}\t{}\n'.format(name, path))
 
+    #original_plan = call_cactus_prepare()
 
-    commands = call_cactus_prepare()
+
+def cactus_aligment_update(options):
+    print(options)    
+
+
+
+def add_subcommand_options(subparser, parent_parser, subcommand):
     
+    if 'node' in subcommand:
+        parser_node_approach = subparser.add_parser("node", parents=[parent_parser], help='Adding a new genome to a node (aka, the update-node approach)')
+        requiredNamed = parser_node_approach.add_argument_group('required named arguments')
+        
+        # required args for subcommand "node"
+        requiredNamed.add_argument("--genome", "-g", help="Name of the genome in the existing alignment", required=True, metavar='GENOME_NAME')
+
+    elif 'branch' in subcommand:
     
+        parser_branch_approach = subparser.add_parser("branch", parents=[parent_parser], help='Add a new genome to a branch (aka, the update-branch approach)')
+        requiredNamed = parser_branch_approach.add_argument_group('required named arguments')
     
+        # required args for subcommand "branch"
+        requiredNamed.add_argument("--topGenome", "-tg", help="Name of the genome in the existing alignment",dest='parent_genome', required=True, metavar='GENOME_NAME')
+        requiredNamed.add_argument("--bottomGenome", "-bg", help="Name of the genome in the existing alignment",dest='child_genome', required=True, metavar='GENOME_NAME')
+
 
 def main():
     #print("children: {}".format(halStats_call_get_children("/Users/thiagogenez/Documents/buckets/adding-genome/lepidoptera/runs/0/steps/Anc05.hal", "Anc05")))
 
     # Same main parser as usual
-    parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
+    parser = ArgumentParser(usage='%(prog)s {node,branch}', add_help=False)
+    parser._positionals.title = "subcommand"
 
     # Same subparsers as usual
-    subparsers = parser.add_subparsers(
-        help='Desired action to perform the alignment update', dest='action')
+    subparser = parser.add_subparsers(
+        help='Desired alignment update approach', dest='action')
 
     # Usual arguments which are applicable for the whole script / top-level args
-    parser.add_argument('--verbose', help='Common top-level parameter',
-                        action='store_true', required=False)
+    #parser.add_argument('--verbose', help='Common top-level parameter',
+    #                    action='store_true', required=False)
 
     # Create parent subparser. Note `add_help=False` and creation via `argparse.`
     parent_parser = ArgumentParser(
@@ -197,9 +239,7 @@ def main():
     parent_parser.add_argument(
         "inHal", help="The input HAL-format file containing the existing alignment")
     parent_parser.add_argument(
-        "genomeName", help="The name of the new genome that is being added")
-    parent_parser.add_argument(
-        "fastaFile", help="The FASTA file of the new genome")
+        "inFasta", help="Tab-separated file. First column: genome name, second column: FASTA-format file path")
 
     # taken from cactus-prepare
     parent_parser.add_argument(
@@ -209,26 +249,19 @@ def main():
     parent_parser.add_argument(
         "--outSeqFile", help="Path for annotated Seq file output [default: outDir/seqFile]")
 
-    # Subparsers based on parent & Add some arguments exclusively for parser_create
-    parser_create = subparsers.add_parser("node",  aliases=['no'], parents=[
-                                          parent_parser], help='Adding a new genome to a node (aka, the update-node approach)')
-    requiredNamed = parser_create.add_argument_group(
-        'required named arguments')
-    requiredNamed.add_argument(
-        "--genome", "-g", help="Name of the genome in the existing alignment", required=True, metavar='GENOME_NAME')
-
-    parser_update = subparsers.add_parser("branch", aliases=['br'], parents=[
-                                          parent_parser], help='Add a new genome to a branch (aka, the update-branch approach)')
-    requiredNamed = parser_update.add_argument_group(
-        'required named arguments')
-    requiredNamed.add_argument("--topGenome", "-tg", help="Name of the genome in the existing alignment",
-                               dest='parent_genome', required=True, metavar='GENOME_NAME')
-    requiredNamed.add_argument("--bottomGenome", "-bg", help="Name of the genome in the existing alignment",
-                               dest='child_genome', required=True, metavar='GENOME_NAME')
+    # add subcommands options
+    add_subcommand_options(subparser, parent_parser, 'node')
+    add_subcommand_options(subparser, parent_parser, 'branch')
 
     options = parser.parse_args()
 
-    print(options)
+    if not options.action:
+        parser.print_help()
+        sys.exit(1)
+    
+    
+    cactus_aligment_update(options)
+        
 
 
 if __name__ == '__main__':
