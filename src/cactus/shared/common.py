8ae2d847560130256211a94ab66163e8e7aecd88
Glenn Hickey <glenn.hickey@gmail.com>
2023-01-09 10:49:19 -0500

    prep release v2.4.0

--- src/cactus/shared/common.py
+++ src/cactus/shared/common.py
@@ -305,7 +305,7 @@ def getDockerImage():
 
 def getDockerRelease(gpu=False):
     """Get the most recent docker release."""
-    r = "quay.io/comparative-genomics-toolkit/cactus:v2.3.1"
+    r = "quay.io/comparative-genomics-toolkit/cactus:v2.4.0"
     if gpu:
         r += "-gpu"
     return r
