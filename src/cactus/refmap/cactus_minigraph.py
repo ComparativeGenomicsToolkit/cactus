3fe7083b71a893cfd96235b9cf60d88fea6ab667
Glenn Hickey <glenn.hickey@gmail.com>
2023-07-01 21:20:55 -0400

    fix last commit

--- src/cactus/refmap/cactus_minigraph.py
+++ src/cactus/refmap/cactus_minigraph.py
@@ -132,8 +132,8 @@ def check_sample_names(sample_names, references):
         if type(references) is str:
             references = [references]
         if references[0] not in sample_names:
-                raise RuntimeError("Specified reference, \"{}\" not in seqfile".format(references[0]))
-
+            raise RuntimeError("Specified reference, \"{}\" not in seqfile".format(references[0]))
+        for reference in references:
             # graphmap-join uses reference names as prefixes, so make sure we don't get into trouble with that
             reference_base = os.path.splitext(reference)[0]
             for sample in sample_names:
