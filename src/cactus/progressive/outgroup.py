c283b9a92363d0fd941fbdf55ea034020fc24609
Glenn Hickey <glenn.hickey@gmail.com>
2024-01-16 20:53:07 -0500

    start simple greedy outgroup sex-chrom solver

--- src/cactus/progressive/outgroup.py
+++ src/cactus/progressive/outgroup.py
@@ -11,6 +11,7 @@ leaves as outgroups
 """
 
 import os
+import sys
 import math
 import copy
 import itertools
@@ -24,6 +25,7 @@ from cactus.shared.configWrapper import ConfigWrapper
 from cactus.progressive.multiCactusTree import MultiCactusTree
 from cactus.progressive.seqFile import SeqFile
 from cactus.shared.common import cactus_call
+from toil.statsAndLogging import logger
 
 class GreedyOutgroup(object):
     def __init__(self):
@@ -33,20 +35,30 @@ class GreedyOutgroup(object):
         self.root = None
         self.ogMap = None
         self.mcTree = None
+        self.chrom_map = None
 
     # add edges from sonlib tree to self.dag
     # compute self.dm: an undirected distance matrix
     def importTree(self, mcTree, rootId = None):
+        # input tree
         self.mcTree = mcTree
+        # initialize digraph with input tree
         self.dag = mcTree.nxDg.copy()
+        # root of input tree
         self.root = mcTree.rootId
+        # remove nodes that aren't events (probably unused in practice)
         self.stripNonEvents(self.root, mcTree.subtreeRoots)
+        # distance matrix of tree (directed)
         self.dmDirected = dict(NX.algorithms.shortest_paths.weighted.\
         all_pairs_dijkstra_path_length(self.dag))
+        # nodes that are outside scope and should just be ignored
         self.invalidSet = self.getInvalid(rootId)
+        # undirected graph
         graph = NX.Graph(self.dag)
+        # undirected distance matrix
         self.dm = dict(NX.algorithms.shortest_paths.weighted.\
         all_pairs_dijkstra_path_length(graph))
+        # the results: map tree events to otugroups
         self.ogMap = defaultdict(list)
 
     # return set of ancestral nodes that aren't below alignment root
@@ -108,7 +120,7 @@ class GreedyOutgroup(object):
                 htable[node] = max([htable[i] for i in children]) + 1
         rec(self.root)
         return htable
-
+    
     # check the candidate using the set and and fraction
     def inCandidateSet(self, node, candidateChildFrac):
         if self.candidateMap is None or len(self.candidateMap) == 0:
@@ -134,6 +146,103 @@ class GreedyOutgroup(object):
         self.candidateMap[self.mcTree.getName(node)] = False
         return False
 
+    def loadChromInfo(self, chrom_info_name):
+        """
+        load the chrom info.  it's like a seqfile, but maps genome names
+        to lists of chromosomes.  not all genomes need to be in it
+        """
+        self.chrom_map = {}
+        
+        if not os.path.isfile(chrom_info_name):
+            raise RuntimeError('Unable to open chromosome info file {}'.format(chrom_info_name))
+
+        with open(chrom_info_name, 'r') as chrom_info_file:
+            for line in chrom_info_file:
+                toks = line.rstrip().split()
+                if len(toks) == 2:
+                    genome_name = toks[0]
+                    chroms = toks[1]
+                    self.chrom_map[genome_name] = chroms.split(',')
+                elif len(toks):
+                    raise RuntimeError('Unable to parse line in {}, expecting 2 columns: {}'.format(chrom_info_name, line))
+                    
+    def check_chrom_satisfied(self, node_id, id_to_chroms):
+        """
+        see if the current outgroup selection for the given node has at least
+        one copy of every chromosome in/under that node.  this check is only
+        relevant if input chroms have been specified...
+
+        these chromosomes are strings like "X", "Y" etc.  that can
+        be used to denote the sex of certain leaf assemblies.  This
+        will then be used to guide outgroup selection, where outgroups
+        are chosen so that at least two copies of each chromosome
+        are present in the ingroup+outgroup set.  
+
+        """
+        node_chrom_set = id_to_chroms[node_id]
+        if not node_chrom_set:
+            return True
+        outgroup_names = self.ogMap[self.mcTree.getName(node_id)]
+        og_ids = [self.mcTree.getNodeId(og_name[0]) for og_name in outgroup_names]
+        og_chroms = set()
+        for og_id in og_ids:
+            for og_chrom in id_to_chroms[og_id]:
+                og_chroms.add(og_chrom)
+        return node_chrom_set.issubset(og_chroms)
+
+    def refine_og_chroms(self, node_chroms, max_outgroups):
+        """
+        go over the outgroup assignment and try to refine the selections to
+        best represent the chromosome specificaitons
+        """
+        for source_name, og_names_dists in self.ogMap.items():
+            source_id = self.mcTree.getNodeId(source_name)
+            source_chroms = node_chroms[source_id]            
+            # only need to do anything if we have chromosomes
+            if source_chroms:
+                chrom_set = set()
+                essential_ogs = []
+                for og_name, og_dist in og_names_dists:
+                    og_id = self.mcTree.getNodeId(og_name)
+                    og_chroms = node_chroms[og_id]
+                    is_essential = False
+                    for og_chrom in og_chroms:
+                        if og_chrom in source_chroms and og_chrom not in chrom_set:
+                            # this outgroup brings with it a needed chromosome we haven't seen yet,
+                            # so we mark it as "essential"
+                            is_essential = True
+                            chrom_set.add(og_chrom)
+                    if is_essential:
+                        essential_ogs.append((og_name, og_dist))
+
+                # todo: we can/should use some kind of threshold to make sure we
+                # don't take rediculously distance outgroups just because they
+                # have the right chromosomes
+                if len(essential_ogs) > max_outgroups:
+                    logger.warning("Warning: Limiting outgroups for {} to {}, which means leaving out {}".format(
+                        source_name, max_outgroups, ",".join([dog[0] for dog in essential_ogs[max_outgroups:]])))
+                    # todo: rank by how many chromosomes they have!
+                    essential_ogs = essential_ogs[:max_outgroups]
+                
+                if len(chrom_set) < len(source_chroms):
+                    logger.warning("Warning: Unable to fulfull chromosome requirements when selecting outgroups for {}".format(source_name) +
+                                   ". In particular, these chromosomes could not be found: {}".format(",".join(source_chroms - chrom_set)))
+                # if we have room for outgroups that aren't listed "essential" for their chromosome content,
+                # then pad by distance
+                if len(essential_ogs) < max_outgroups:
+                    padded_ogs = []
+                    padding = max_outgroups - len(essential_ogs)
+                    for og_name, og_dist in og_names_dists:
+                        is_essential = (og_name, og_dist) in essential_ogs
+                        if is_essential or padding > 0:
+                            padded_ogs.append((og_name, og_dist))
+                            if not is_essential:
+                                padding -= 1
+                    essential_ogs = padded_ogs
+
+                # update the outgroupss
+                self.ogMap[source_name] = essential_ogs                    
+
     # greedily assign closest possible valid outgroups
     # If some outgroups are already assigned, keep the existing
     # assignments but attempt to add more, if possible.
@@ -150,8 +259,13 @@ class GreedyOutgroup(object):
     # if > 1, then only members of the candidate set and none of their
     # ancestors are chosen
     # maxNumOutgroups : max number of outgroups to put in each entry of self.ogMap
+    # maxNumChromOutgroups : max number of outgroups when trying to make chromosome assignment
+    # should be bigger than maxNumOutgroups so that the genomes with rare chromosomes can
+    # get the extra help they need.
     def greedy(self, threshold = None, candidateSet = None,
-               candidateChildFrac = 2., maxNumOutgroups = 1):
+               candidateChildFrac = 2., maxNumOutgroups = 1,
+               maxNumChromOutgroups = 1):
+        # sort the (undirected) distance map
         orderedPairs = []
         for source, sinks in list(self.dm.items()):
             for sink, dist in list(sinks.items()):
@@ -167,62 +281,104 @@ class GreedyOutgroup(object):
 
         htable = self.heightTable()
 
+        # convert the input (leaf) chroms to id-space
+        node_chroms = defaultdict(set)
+        if self.chrom_map:
+            for node_name, chr_list in self.chrom_map.items():
+                node_chroms[self.mcTree.getNodeId(node_name)] = set(chr_list)
+                
+        # fill in the chromosomes of the ancestors, where they are (maybe naively)
+        # just the union of all chroms below
+        # these are the chroms that outgroups will need to have if possible.
+        for node, chr_list in list(node_chroms.items()):
+            parent = node
+            while (self.mcTree.hasParent(parent)):
+                parent = self.mcTree.getParent(parent)
+                prev_len = len(node_chroms[parent])
+                node_chroms[parent] = node_chroms[parent].union(chr_list)
+                if len(node_chroms[parent]) == prev_len:
+                    # no sense continuing traveral upwards if we're not adding anything
+                    break
+
+        # fast check to see if chromosomes of a given source are satisfied
+        source_satisfied = {}
+        for node in self.mcTree.postOrderTraversal():
+            if node in node_chroms and len(node_chroms[node]) > 0 and not self.mcTree.isLeaf(node):
+                # ancestral nodes with descendant chromosome specificaitons need to be satisfied
+                source_satisfied[node] = False
+            else:
+                source_satisfied[node] = True
+
+        # sort the orderedPairs by source
+        ordered_pairs_by_source = defaultdict(list)
         for candidate in orderedPairs:
+            # source is the ancestor we're trying to find outgroups for
             source = candidate[1][0]
-            sink = candidate[1][1]
-            sourceName = self.mcTree.getName(source)
-            sinkName = self.mcTree.getName(sink)
-            dist = candidate[0]
+            ordered_pairs_by_source[source].append(candidate)
 
-            # skip leaves (as sources)
-            if len(self.dag.out_edges(source)) == 0:
-                finished.add(source)
+        # visit the tree bottom up
+        for node in self.mcTree.postOrderTraversal():
+            # visit the candidates in order of increasing distance
+            orderedPairs = ordered_pairs_by_source[node]
+            for candidate in orderedPairs:
+                # source is the ancestor we're trying to find outgroups for
+                source = candidate[1][0]
+                # sink it the candidate outgroup
+                sink = candidate[1][1]
+                sourceName = self.mcTree.getName(source)
+                sinkName = self.mcTree.getName(sink)
+                dist = candidate[0]
 
-            # skip nodes that were already finished in a previous run
-            if sourceName in self.ogMap and len(self.ogMap[sourceName]) >= maxNumOutgroups:
-                finished.add(source)
+                # skip leaves (as sources)
+                if len(self.dag.out_edges(source)) == 0:
+                    finished.add(source)
 
-            # skip invalid outgroups
-            if sink in self.invalidSet:
-                continue
+                # skip nodes that were already finished in a previous run
+                if sourceName in self.ogMap and source_satisfied[source] and len(self.ogMap[sourceName]) >= maxNumOutgroups:
+                    finished.add(source)
 
-            # skip nodes that aren't in the candidate set (if specified)
-            # or don't have enough candidate children
-            if not self.inCandidateSet(sink, candidateChildFrac):
-                continue
+                # skip invalid outgroups
+                if sink in self.invalidSet:
+                    continue
 
-            # candidate (ancestral) sink is too high up the tree compared to the source, so we skip
-            if (threshold is not None and
-                not self.mcTree.isLeaf(sink) and
-                # "+ 1" because we want a threshold of 0 to indicate we
-                # probably won't have to wait for the outgroup (it will
-                # already have been finished, assuming all subproblems
-                # proceed at the same rate)
-                htable[sink] - htable[source] + 1 > threshold):
-                continue
+                # skip nodes that aren't in the candidate set (if specified)
+                # or don't have enough candidate children
+                if not self.inCandidateSet(sink, candidateChildFrac):
+                    continue
 
-            # Don't use any outgroups that are a child of another node
-            # already in the outgroup set
-            if any([self.onSamePath(x, sink) for x in self.dag.successors(source)]):
-                continue
+                # candidate (ancestral) sink is too high up the tree compared to the source, so we skip
+                if (threshold is not None and
+                    not self.mcTree.isLeaf(sink) and
+                    # "+ 1" because we want a threshold of 0 to indicate we
+                    # probably won't have to wait for the outgroup (it will
+                    # already have been finished, assuming all subproblems
+                    # proceed at the same rate)
+                    htable[sink] - htable[source] + 1 > threshold):
+                    continue
 
-            if source not in finished and \
-            not self.onSamePath(source, sink):
-                self.dag.add_edge(source, sink, weight=dist, info='outgroup')
-                if NX.is_directed_acyclic_graph(self.dag):
-                    htable[source] = max(htable[source], htable[sink] + 1)
-                    existingOutgroups = [i[0] for i in self.ogMap[sourceName]]
-                    if sinkName in existingOutgroups:
-                        # This outgroup was already assigned to this source in a previous run
-                        # Sanity check that the distance is equal
-                        existingOutgroupDist = dict(self.ogMap[sourceName])
-                        assert existingOutgroupDist[sinkName] == dist
-                        continue
-                    self.ogMap[sourceName].append((sinkName, dist))
-                    if len(self.ogMap[sourceName]) >= maxNumOutgroups:
-                        finished.add(source)
-                else:
-                    self.dag.remove_edge(source, sink)
+                # Don't use any outgroups that are a child of another node
+                # already in the outgroup set
+                if any([self.onSamePath(x, sink) for x in self.dag.successors(source)]):
+                    continue
+
+                if source not in finished and \
+                not self.onSamePath(source, sink):
+                    self.dag.add_edge(source, sink, weight=dist, info='outgroup')
+                    if NX.is_directed_acyclic_graph(self.dag):
+                        htable[source] = max(htable[source], htable[sink] + 1)
+                        existingOutgroups = [i[0] for i in self.ogMap[sourceName]]
+                        if sinkName in existingOutgroups:
+                            # This outgroup was already assigned to this source in a previous run
+                            # Sanity check that the distance is equal
+                            existingOutgroupDist = dict(self.ogMap[sourceName])
+                            assert existingOutgroupDist[sinkName] == dist
+                            continue
+                        self.ogMap[sourceName].append((sinkName, dist))
+                        source_satisfied[source] = self.check_chrom_satisfied(source, node_chroms)
+                        if len(self.ogMap[sourceName]) >= maxNumOutgroups and source_satisfied[source]:
+                            finished.add(source)
+                    else:
+                        self.dag.remove_edge(source, sink)
 
         # Since we could be adding to the ogMap instead of creating
         # it, sort the outgroups by distance again. Sorting the
@@ -230,6 +386,11 @@ class GreedyOutgroup(object):
         # work well.
         for node, outgroups in list(self.ogMap.items()):
             self.ogMap[node] = sorted(outgroups, key=lambda x: x[1])
+
+        # the chromosome specification logic trumps the maximum number of outgroups
+        # so we do a second pass to reconcile them (greedily) as best as possible
+        if node_chroms:
+            self.refine_og_chroms(node_chroms, max(maxNumOutgroups, maxNumChromOutgroups))
 
 
 def main():
@@ -242,6 +403,8 @@ def main():
                       default = None, help="greedy threshold")
     parser.add_option("--numOutgroups", dest="maxNumOutgroups",
                       help="Maximum number of outgroups to provide", type=int)
+    parser.add_option("--chromInfo", dest="chromInfo",
+                      help="File mapping genomes to sex chromosome lists")
     parser.add_option("--configFile", dest="configFile",
                       help="Specify cactus configuration file",
                       default=os.path.join(cactusRootPath(), "cactus_progressive_config.xml"))    
@@ -264,6 +427,8 @@ def main():
 
     outgroup = GreedyOutgroup()
     outgroup.importTree(mc_tree)
+    if options.chromInfo:
+        outgroup.loadChromInfo(options.chromInfo)
     if options.justLeaves:
         candidates = set([mc_tree.getName(x)
                         for x in mc_tree.getLeaves()])
--- src/cactus/progressive/outgroupTest.py
+++ src/cactus/progressive/outgroupTest.py
@@ -57,6 +57,22 @@ class TestCase(unittest.TestCase):
         self.backboneTree.nameUnlabeledInternalNodes()
         self.mcTrees.append(self.backboneTree)
 
+        # sort of realistic diploid t2t primates tree
+        dipApesTree = '((((gor.1:0.0001,gor.2:0.0001):0.00993,(((chimp.1:0.0002,chimp.2:0.0001):0.00272,(bonobo.1:0.0001,bonobo.2:0.0001):0.00269):0.00415,(hs1:0.00025,hg38:0.00025):0.00619):0.00046):0.00509,((ponabe.1:0.0002,ponabe.2:0.0001):0.000945412,(ponpy.1:0.0001,ponpy.2:0.0001):0.000915022):0.01864):0.000107106,(gibbon.1:0.0001,gibbon.2:0.002):0.00990798);'
+        self.dipApesMcTree = MultiCactusTree(NXNewick().parseString(dipApesTree, addImpliedRoots=False))
+        self.dipApesMcTree.computeSubtreeRoots()
+        self.dipApesMcTree.nameUnlabeledInternalNodes()
+        self.mcTrees.append(self.borMcTree)        
+        self.dipApesChromInfo = os.path.join(self.tempDir, 'chrom-info.txt')
+        with open(self.dipApesChromInfo, 'w') as chrom_file:
+            chrom_file.write('gor.1 x\ngor.2 y\n')
+            chrom_file.write('chimp.1 x,y\n')
+            chrom_file.write('bonobo.1 x\nbonobo.2 y\n')
+            chrom_file.write('hs1 x,y\nhg38 y,x\n')
+            chrom_file.write('ponabe.1 x,y\n')
+            chrom_file.write('ponpy.1 x,y\n')
+            chrom_file.write('gibbon.1 x,y\n')            
+
         seqLens = dict()
         seqLens["HUMAN"] = 57553
         seqLens["CHIMP"] = 57344
@@ -305,6 +321,39 @@ class TestCase(unittest.TestCase):
                             sink2Id = tree.nameToId[sink2]
                             assert sink1Id not in tree.postOrderTraversal(sink2Id)
                             assert sink2Id not in tree.postOrderTraversal(sink1Id)
+
+    @TestStatus.shortLength
+    def testDiploidPrimatesChromSpec(self):
+        """No two outgroups should be on the same path to the root."""
+        tree = self.dipApesMcTree
+        tree.nameUnlabeledInternalNodes()
+        og = GreedyOutgroup()
+        og.importTree(tree)
+        og.loadChromInfo(self.dipApesChromInfo)
+        og.greedy(maxNumOutgroups=1)
+        chrom_map = {}
+        for source_name, ogs_dists in og.ogMap.items():
+            chrom_map[source_name] = [o[0] for o in ogs_dists]
+
+        og = GreedyOutgroup()
+        og.importTree(tree)
+        og.greedy(maxNumOutgroups=1)
+        default_map = {}
+        for source_name, ogs_dists in og.ogMap.items():
+            default_map[source_name] = [o[0] for o in ogs_dists]
+
+        self.assertEqual(len(chrom_map), len(default_map))
+        self.assertEqual(sorted(chrom_map.keys()), sorted(default_map.keys()))
+        for source, og in default_map.items():
+            assert source in chrom_map
+            # the idea is that even though .2 is closer (so it gets chosen in default)
+            # .1 has the desired chroms, so it shuold be chosen when the chrom map is used
+            self.assertEqual(len(og), 1)
+            if og[0].startswith('chimp') or og[0].startswith('ponabe') or og[0].startswith('gibbon'):
+                self.assertEqual([og[0].replace('.2', '.1')], chrom_map[source])
+            else:
+                self.assertEqual(og, chrom_map[source])
+                            
 def main():
     unittest.main()
 
