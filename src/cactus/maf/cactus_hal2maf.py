2ffb82147d95efe5db1389beb27b47c6f6ca28ab
Glenn Hickey <glenn.hickey@gmail.com>
2025-08-22 09:29:14 -0400

    dont log sort memory

--- src/cactus/maf/cactus_hal2maf.py
+++ src/cactus/maf/cactus_hal2maf.py
@@ -448,7 +448,7 @@ def taf_cmd(hal_path, chunk, chunk_num, genome_list_path, sed_script_paths, opti
         # rename to alphanumeric names
         cmd += ' | sed -f {}'.format(os.path.basename(sed_script_paths[0]))
     if genome_list_path:
-        cmd += ' | taffy view | {} taffy sort -n {}{} | taffy view -m 2> {}.sort.time'.format(time_cmd, os.path.basename(genome_list_path), time_end, chunk_num)        
+        cmd += ' | taffy view | taffy sort -n {} | taffy view -m'.format(os.path.basename(genome_list_path), chunk_num)        
     if options.dupeMode == 'single':
         cmd += ' | mafDuplicateFilter -m - -k'                                               
     elif options.dupeMode == 'consensus':
@@ -613,7 +613,7 @@ def hal2maf_batch(job, hal_id, batch_chunks, genome_list, options, config):
         except Exception as e:
             logger.error("Parallel taffy command failed, dumping all stderr")
             for chunk_num in range(len(batch_chunks)):
-                for tag, cmd in [('sort', 'sort'), ('tn', 'norm')]:                
+                for tag, cmd in [('tn', 'norm')]:                
                     stderr_file_path = os.path.join(work_dir, '{}.{}.time'.format(chunk_num, tag))
                     if os.path.isfile(stderr_file_path):
                         with open(stderr_file_path, 'r') as stderr_file:
@@ -628,7 +628,7 @@ def hal2maf_batch(job, hal_id, batch_chunks, genome_list, options, config):
             cmd_toks = taf_cmds[chunk_num].split(' ')
             for i in range(len(cmd_toks)):
                 cmd_toks[i] = cmd_toks[i].rstrip(')')
-            for tag, cmd in [('sort', 'sort'), ('tn', 'norm')]:
+            for tag, cmd in [('tn', 'norm')]:
                 tag_start = None
                 if cmd in cmd_toks:
                     tag_start = cmd_toks.index(cmd) - 1
