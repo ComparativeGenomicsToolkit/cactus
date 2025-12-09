a5fa6c13f5b352e7a5886767b774aae6443a82cf
Glenn Hickey <glenn.hickey@gmail.com>
2025-12-09 08:12:53 -0500

    Prep release 3.1.1

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open(versionFile, 'w') as versionFH:
 
 setup(
     name = "Cactus",
-    version = "3.1.0",
+    version = "3.1.1",
     author = "Benedict Paten",
     package_dir = {'': 'src'},
     packages = find_packages(where='src'),
